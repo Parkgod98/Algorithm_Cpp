045a19469992a1beca34060aca94ebf56f4cc1ab
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-02 19:27:34 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+//int main(void)
+//{
+//	int n, m;
+//	scanf("%d %d", &n, &m);
+//
+//	int* column = (int*)malloc(sizeof(int) * n);
+//}
+
+int main(void)
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a * b);
+}
