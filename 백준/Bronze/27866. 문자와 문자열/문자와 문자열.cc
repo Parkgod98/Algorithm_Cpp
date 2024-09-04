c007bb39e7d28e1de9a0b946a8fa658eb3d82ac2
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-04 14:15:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+	int i;
+
+	cin >> s >> i;
+	cout << s[i - 1];
+}
