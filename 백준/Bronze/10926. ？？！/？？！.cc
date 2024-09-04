7fac0c7e58bf49ad8a62a3ef9d266808d8cebc87
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-04 12:50:32 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 100
+
+int main(void)
+{
+	char arr[SIZE];
+	scanf("%s", arr);
+
+	printf("%s??!", arr);
+}
