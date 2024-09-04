915b35e167edc876b84a06c2f1463960da305511
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-04 14:03:56 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+
+	cin >> s;
+	cout << s.length();
+
+	return 0;
+}
