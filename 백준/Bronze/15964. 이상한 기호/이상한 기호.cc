6d86b20c9d863c5a16d75f37e575185323f0dd81
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-05 16:48:05 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,14 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << (a + b) * (a - b) << "\n";
+}
