eb941d1f5a67f1be4ae47a026201e33fbfe19ece
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-05 16:30:36 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+	long long n, m;
+	
+	cin >> n >> m;
+
+	cout << abs(n - m) << "\n";
+}
