039bfb52727a19a69e6e719407fe960ac9b66a8d
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-05 16:03:40 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main()
@@ -10,43 +11,21 @@ int main()
 
 	cin >> a >> b;
 
-	int a_len = a.length();
-	int b_len = b.length();
-	int max_len, min_len;
-	
-	if (a_len >= b_len) {
-		max_len = a_len;
-		min_len = b_len;
-	}
-	else {
-		max_len = b_len;
-		min_len = a_len;
-		string tmp = a;
-		a = b;
-		b = tmp;
-	}
+	int max_len = max(a.length(), b.length());
+	a = string(max_len - a.length(), '0') + a;
+	b = string(max_len - b.length(), '0') + b;
 
-	string mask = "";
-	for (int i = 0; i < max_len - min_len; ++i)
-		mask += "0";
-	b = mask + b;
-
-	string result = "";
 	int carry = 0;
+	string result = "";
 	for (int i = max_len - 1; i >= 0; --i) {
-		int value = (a[i]-'0') + (b[i]-'0') + carry;
-		if (value >= 10) {
-			result += char(value % 10) + '0';
-			carry = value / 10;
-		}
-		else {
-			result += char(value) + '0';
-			carry = 0;
-		}
+		int value = (a[i] - '0') + (b[i] - '0') + carry;
+		carry = value / 10;
+		result += ((value % 10) + '0');
 	}
 	if (carry != 0)
-		result += char(carry) + '0';
+		result += ((carry % 10) + '0');
 
 	reverse(result.begin(), result.end());
 	cout << result;
+
 }
