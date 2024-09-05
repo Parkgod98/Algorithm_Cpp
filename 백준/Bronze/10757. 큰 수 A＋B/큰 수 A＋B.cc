ae234da06c80b746dd5b33f04ae77d4a8765f994
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-05 15:42:06 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -0,0 +1,64 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string ReverseS(string s) {
+	int len = s.length();
+
+	for (int i = 0; i < len / 2; ++i) {
+		char tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+
+	return s;
+}
+
+int main()
+{
+	string a, b;
+
+	cin >> a >> b;
+
+	int a_len = a.length();
+	int b_len = b.length();
+	int max_len, min_len;
+	
+	if (a_len >= b_len) {
+		max_len = a_len;
+		min_len = b_len;
+	}
+	else {
+		max_len = b_len;
+		min_len = a_len;
+		string tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	string mask = "";
+	for (int i = 0; i < max_len - min_len; ++i)
+		mask += "0";
+	b = mask + b;
+
+	string result = "";
+	int carry = 0;
+	for (int i = max_len - 1; i >= 0; --i) {
+		int value = (a[i]-'0') + (b[i]-'0') + carry;
+		if (value >= 10) {
+			result += char(value % 10) + '0';
+			carry = value / 10;
+		}
+		else {
+			result += char(value) + '0';
+			carry = 0;
+		}
+	}
+	if (carry != 0)
+		result += char(carry) + '0';
+
+	cout << ReverseS(result);
+}
+
