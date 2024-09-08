c27e474e33fff45140b27a15cd1cdf84c394ca61
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-08 16:44:06 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int main()
+{
+	int r1, s;
+	cin >> r1 >> s;
+	cout << s * 2 - r1 << "\n";
+}
