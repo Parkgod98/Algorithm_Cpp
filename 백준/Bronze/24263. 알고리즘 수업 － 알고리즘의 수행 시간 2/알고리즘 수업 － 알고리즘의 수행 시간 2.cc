dd24ea9f4e2330411b04846151cbcac0a1a13350
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-08 17:20:04 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int main()
+{
+	int n;
+	cin >> n;
+	
+	cout << n << "\n" <<1;
+}
