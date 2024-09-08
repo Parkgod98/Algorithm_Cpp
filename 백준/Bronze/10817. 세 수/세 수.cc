f21b53857a558d69de7d506b8f43646ea8bc5ab7
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-08 19:21:06 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int main()
+{
+	vector<int> v;
+	for (int i = 0; i < 3; ++i) {
+		int n;
+		cin >> n;
+		v.push_back(n);
+	}
+	sort(v.begin(), v.end());
+	cout << v[1] << endl;
+}
