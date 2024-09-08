40f7fc87d36a96c394a010d0fa7423412289c595
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-08 19:54:03 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		for (int s = 0; s < n - i -1; ++s)
+			cout << " ";
+		for (int k = 0; k < 2 * i + 1; ++k)
+			cout << "*";
+		cout << endl;
+	}
+	for (int i = 0; i < n - 1; ++i) {
+		for (int s = 0; s < i + 1; ++s)
+			cout << " ";
+		for (int k = 0; k < 2 *n -2*(i+1) -1; ++k)
+			cout << "*";
+		cout << endl;
+	}
+}
+	
