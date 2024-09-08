d5481c8936e0e9a3122e6d9ff6fddc4c7f328907
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-09 00:58:07 +0900

    [Bronze II] Title: 단어의 개수, Time: 1004 ms, Memory: 12108 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -15,10 +15,12 @@ vector<string> split(string input, string delimeter) {
 	long long pos = 0;
 	while ((pos = input.find(delimeter)) != string::npos) {
 		token = input.substr(0, pos);
-		ret.push_back(token);
+		if (token != "")
+			ret.push_back(token);
 		input.erase(0, pos + delimeter.length());
 	}
-	ret.push_back(input);
+	if (input != "")
+		ret.push_back(input);
 
 	return ret;
 }
@@ -29,12 +31,6 @@ int main()
 	getline(cin, s);
 
 	vector<string> res = split(s, " ");
-	
-	int sum = 0;
-	for (string s : res) {
-		if (s != "")
-			sum += 1;
-	}
-	cout << sum;
+	cout << res.size();
 }
 	
