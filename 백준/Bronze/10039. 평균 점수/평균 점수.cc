4548e9d8bce9a1bc8bcad208383f5e088906e389
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-08 16:42:49 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 5; ++i) {
+		int n;
+		cin >> n;
+		if (n < 40)
+			n = 40;
+		sum += n;
+	}
+	cout << sum / 5 << "\n";
+}
