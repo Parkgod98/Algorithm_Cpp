1fa4173d8979bd01b8035777e4acc76dea51aa64
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-13 14:44:23 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -13,23 +13,17 @@ using namespace std;
 
 int main()
 {
+	vector<int> coin = { 500,100,50,10,5,1 };
+
 	int n;
 	cin >> n;
-
 	n = 1000 - n;
 
 	int res = 0;
-	res += n / 500;
-	n %= 500;
-	res += n / 100;
-	n %= 100;
-	res += n / 50;
-	n %= 50;
-	res += n / 10;
-	n %= 10;
-	res += n / 5;
-	n %= 5;
-	res += n;
+	for (int i = 0; i < coin.size(); ++i) {
+		res += n / coin[i];
+		n %= coin[i];
+	}
 
 	cout << res;
 }
