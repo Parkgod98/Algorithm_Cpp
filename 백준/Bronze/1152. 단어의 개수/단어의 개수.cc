9b549a656652d025b215f60f99abac52f86d60c8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-13 13:16:51 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -3,34 +3,27 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <cmath>
 #include <map>
 using namespace std;
 
 //ios_base::sync_with_stdio(false);
 //cin.tie(NULL);
 
-vector<string> split(string input, string delimeter) {
-	vector<string> ret;
-	string token = "";
-	long long pos = 0;
-	while ((pos = input.find(delimeter)) != string::npos) {
-		token = input.substr(0, pos);
-		if (token != "")
-			ret.push_back(token);
-		input.erase(0, pos + delimeter.length());
-	}
-	if (input != "")
-		ret.push_back(input);
-
-	return ret;
-}
-
 int main()
 {
+	char c, pc;
+	pc = ' ';
 	string s;
 	getline(cin, s);
-
-	vector<string> res = split(s, " ");
-	cout << res.size();
-}
 	
+	int len = s.length();
+	int count = 0;
+	for (int i = 0; i < len; ++i) {
+		c = s[i];
+		if (isalpha(c) && isspace(pc))
+			count++;
+		pc = c;
+	}
+	cout << count << "\n";
+}
