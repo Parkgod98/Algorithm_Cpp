168c08dd50b0b03882978196f08786ee4549797a
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-23 21:00:44 +0900

    [Silver IV] Title: 좋은 단어, Time: 0 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -23,9 +23,7 @@ bool IsGood(string s)
 	if (len % 2 == 1)
 		return false;
 	for (int i = 0; i < len; ++i) {
-		if (st.empty())
-			st.push(s[i]);
-		else if (!st.empty() && st.top() == s[i])
+		if (!st.empty() && st.top() == s[i])
 			st.pop();
 		else
 			st.push(s[i]);
