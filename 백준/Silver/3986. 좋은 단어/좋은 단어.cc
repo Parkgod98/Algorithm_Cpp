d9b3308584af2a144860843daac76724f9fc6b87
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-23 20:52:52 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,57 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <stack>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+
+//cout << fixed;
+//cout.precision(2);
+// atoi(s.c_str());
+// stoi()
+
+bool IsGood(string s)
+{
+	stack<char> st;
+	int len = s.size();
+	if (len % 2 == 1)
+		return false;
+	for (int i = 0; i < len; ++i) {
+		if (st.empty())
+			st.push(s[i]);
+		else if (!st.empty() && st.top() == s[i])
+			st.pop();
+		else
+			st.push(s[i]);
+	}
+	if (!st.empty())
+		return false;
+	return true;
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int count = 0;
+	for (int i = 0; i < n; ++i) {
+		string s;
+		cin >> s;
+		if (IsGood(s))
+			count++;
+	}
+
+	cout << count;
+}
