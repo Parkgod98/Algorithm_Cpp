a4d3bd64943bd068ee095c55cd6ee2055fab566b
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-09-24 22:43:55 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+//ios_base::sync_with_stdio(false);
+//cin.tie(NULL);
+//
+//cout << fixed;
+//cout.precision(2);
+// atoi(s.c_str());
+// stoi()
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	cout << n * m - 1;
+}
