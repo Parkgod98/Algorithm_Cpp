2323dd15e0af7391b9e3eb4d6fc02c2157a4c257
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-10-04 16:02:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -25,37 +25,29 @@ int main()
 	int n;
 	cin >> n;
 
-	int ans = 0;
-	if (n % 5 == 0)
-		ans = n / 5;
 	int p = n;
-
 	int five, thr;
 	five = thr = 0;
-	while (five >= 0) {
-		five += p / 5;
-		p %= 5;
-		thr += p / 3;
-		p %= 3;
-		if (p == 0)
-			break;
-		else {
-			while (five >= 0) {
-				five--;
-				p = n;
-				p = p - five * 5;
-				if (p % 3 == 0) {
-					thr = p / 3;
-					break;
-				}
+
+	five += p / 5;
+	p %= 5;
+	thr += p / 3;
+	p %= 3;
+	if (p != 0) {
+		while (five >= 0) {
+			five--;
+			p = n;
+			p = p - five * 5;
+			if (p % 3 == 0) {
+				thr = p / 3;
+				break;
 			}
 		}
-		if (five * 5 + thr * 3 == n)
-			break;
 	}
 	if (five < 0)
 		cout << -1;
 	else
 		cout << five + thr;
+		
 
 }
