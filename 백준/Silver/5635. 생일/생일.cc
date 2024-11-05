4c5bb0c95c1cdbbdfe7e548b8273f2e345c64b75
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-11-05 14:40:03 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.cc
+++ 백준/Silver/5635. 생일/생일.cc
@@ -6,36 +6,16 @@
 #include <algorithm>
 using namespace std;
 
-bool compare(tuple<string, int, int, int> a, tuple<string, int, int, int> b)
-{
-	if (get<3>(a) < get<3>(b))
-		return true;
-	else if (get<3>(a) > get<3>(b))
-		return false;
-	else {
-		if (get<2>(a) < get<2>(b))
-			return true;
-		else if (get<2>(a) > get<2>(b))
-			return false;
-		else {
-			return get<1>(a) < get<1>(b);
-		}
-	}
-}
-
 int main(void)
 {
 	int n;
 	cin >> n;
 	
-	vector<tuple<string, int, int, int>> v(n);
+	vector<pair<pair<int,int>,pair<int,string>>> v(n);
 	for (int i = 0; i < n; ++i) {
-		string s;
-		int d, m, y;
-		cin >> s >> d >> m >> y;
-		v[i] = make_tuple(s, d, m, y);
+		cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first;
 	}
-	sort(v.begin(), v.end(),compare);
+	sort(v.begin(), v.end());
 
-	cout << get<0>(v[v.size() - 1])  << "\n" << get<0>(v[0]);
+	cout << v[n-1].second.second << "\n" << v[0].second.second;
 }
