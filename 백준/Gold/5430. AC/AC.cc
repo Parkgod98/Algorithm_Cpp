320cc954ffe29e1c9af60196d9e8c39b31e7cdb6
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-11-06 22:41:38 +0900

    [Gold V] Title: AC, Time: 804 ms, Memory: 3880 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -37,32 +37,33 @@ int main(void)
 		int r_count = 0;
 		int d_count = 0;
 		for (char c : func) {
+			if (c == 'D')
+				d_count++;
+		}
+		if (d_count > n) {
+			cout << "error" << "\n";
+			continue;
+		}
+		for (char c : func) {
 			if (c == 'R')
 				r_count++;
 			else if (c == 'D') {
-				if (v.size() == 0) {
-					cout << "error" << "\n";
-					error_flag = 1;
-					break;
-				}
 				if (r_count % 2 == 0)
 					v.erase(v.begin());
 				else
 					v.erase(v.end() - 1);
 			}
 		}
-		if (!error_flag) {
-			if (r_count % 2)
-				reverse(v.begin(), v.end());
-			cout << "[";
-			for (int j = 0; j < v.size(); ++j) {
-				if (j != v.size() - 1)
-					cout << v[j] << ",";
-				else
-					cout << v[j];
-			}
-			cout << "]" << "\n";
+		if(r_count%2)
+			reverse(v.begin(),v.end());
+		cout << "[";
+		for (int j = 0; j < v.size(); ++j) {
+			if (j != v.size() - 1)
+				cout << v[j] << ",";
+			else
+				cout << v[j];
 		}
+		cout << "]" << "\n";
 
 	}
 }
