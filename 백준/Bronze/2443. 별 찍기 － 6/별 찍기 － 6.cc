b6c93dfcccc928cd320ef02d01596c76c9070ed4
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-11-24 01:49:55 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void PrintN(char c, int n) {
+	for (int i = 0; i < n; ++i)
+		cout << c;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		PrintN(' ', i);
+		PrintN('*', 2 * n - 2*i - 1);
+		PrintN('\n', 1);
+	}
+}
