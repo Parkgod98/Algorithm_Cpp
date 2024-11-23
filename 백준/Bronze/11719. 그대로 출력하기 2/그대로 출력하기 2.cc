2c185c05ffe476c86b880bf75b8f0c259678c5f7
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-11-24 00:12:52 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	while (getline(cin, s)) {
+		cout << s << endl;
+	}
+}
