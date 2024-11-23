d748f3bbb1299e4d9d1afa542002831e59c7ec08
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-11-23 23:48:27 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << (a + b - c) << "\n";
+	cout << stoi(to_string(a) + to_string(b)) - c;
+}
