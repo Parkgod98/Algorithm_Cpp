9eafd787f73c27149e4ca20321485a02201b0da5
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2024-12-31 02:30:35 +0900

    [Bronze V] Title: X보다 작은 수, Time: 260 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,18 +1,26 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-int main()
+int func3(int N)
+{
+	for (int i = 1; i * i <= N; ++i) {
+		if (i * i == N)
+			return 1;
+	}
+	return 0;
+}
+
+int main(void)
 {
 	int n, x;
-
 	cin >> n >> x;
 
-	int count = 0;
 	for (int i = 0; i < n; ++i) {
 		int k;
 		cin >> k;
 		if (k < x)
 			cout << k << " ";
 	}
+
 }
