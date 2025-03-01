85dd3650c3792a32ec920c43d1f8999227ed1f1c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-03-01 22:56:41 +0900

    [level 0] Title: 커피 심부름, Time: 0.08 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> order) {
+    int answer = 0;
+    for (string &s : order){
+        if (s.find("cafelatte") != string::npos){
+            answer+=5000;
+        }
+        else
+            answer+=4500;
+    }
+    return answer;
+}
