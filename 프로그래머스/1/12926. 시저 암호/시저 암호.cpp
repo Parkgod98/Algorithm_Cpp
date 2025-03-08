f41fec0607a5137c41fe806803f99c8c4e9b0891
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-03-08 13:34:25 +0900

    [level 1] Title: 시저 암호, Time: 0.09 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -7,22 +7,13 @@
 using namespace std;
 
 string solution(string s, int n) {
-	string answer = "";
-	for (int i = 0; i < s.size(); ++i) {
-		if (isalpha(s[i])) {
-			if (islower(s[i])) {
-				if (s[i] + n > 'z')
-					s[i] = s[i] + n - 26;
-				else
-					s[i] = s[i] + n;
-			}
-			else {
-				if (s[i] + n > 'Z')
-					s[i] = s[i] + n - 26;
-				else
-					s[i] = s[i] + n;
-			}
-		}
-	}
-	return s;
+    string answer = "";
+    for (char c : s) {
+        if (isalpha(c)) {
+            char base = islower(c) ? 'a' : 'A';
+            c = (c - base + n) % 26 + base;
+        }
+        answer += c;
+    }
+    return answer;
 }
