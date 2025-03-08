1eac09288c2073c90f199782bce233929f80ff74
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-03-08 20:57:32 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.42 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 
 string solution(vector<int> food) {
-	string answer = "";
-	for (int i = 1; i < food.size(); ++i) {
-		for (int j = 0; j < food[i] / 2; ++j)
-			answer.push_back((char)i +'0');
+	string answer = "0";
+	for (int i = food.size() - 1; i > 0; --i) {
+		for (int j = 0; j < food[i]/2; ++j) {
+			answer = to_string(i) + answer + to_string(i);
+		}
 	}
-	answer.push_back('0');
-	answer += string(answer.rbegin()+1, answer.rend());
 	return answer;
 }
