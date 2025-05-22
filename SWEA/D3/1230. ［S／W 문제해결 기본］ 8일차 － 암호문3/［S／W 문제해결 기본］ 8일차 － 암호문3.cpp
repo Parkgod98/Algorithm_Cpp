6f0e51497fbac74688c574c698b965d3a61e7660
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-05-22 16:28:13 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 38 ms, Memory: 5,964 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.cpp
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.cpp
@@ -1,161 +1,69 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// float b, c;
-// double d, e, f;
-// char g;
-// char var[256];
-// long long AB;
-// cin >> a;                            // int 변수 1개 입력받는 예제
-// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
-// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
-// cin >> g;                            // char 변수 1개 입력받는 예제
-// cin >> var;                          // 문자열 1개 입력받는 예제
-// cin >> AB;                           // long long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// float b = 1.0, c = 2.0;               
-// double d = 3.0, e = 0.0; f = 1.0;
-// char g = 'b';
-// char var[256] = "ABCDEFG";
-// long long AB = 12345678901234567L;
-// cout << a;                           // int 변수 1개 출력하는 예제
-// cout << b << " " << c;               // float 변수 2개 출력하는 예제
-// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
-// cout << g;                           // char 변수 1개 출력하는 예제
-// cout << var;                         // 문자열 1개 출력하는 예제
-// cout << AB;                          // long long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <list>
 
 using namespace std;
 
-struct Node {
-	int val;
-	Node* next;
-};
+int main() {
 
-Node head, tail;
-
-void init() {
-	head.next = nullptr;
-	tail.next = nullptr;
-}
-
-Node* make_new_node(int val) {
-	Node* new_node = new Node();
-	new_node->val = val;
-	new_node->next = nullptr;
-
-	return new_node;
-}
-
-void insert_first(int n) {
-	Node* node = make_new_node(n);
-
-	node->next = head.next;
-	head.next = node;
-	if (tail.next == nullptr) {
-		tail.next = node;
-	}
-}
-
-void insert_interval(int x, int n) {
-	Node* node = make_new_node(n);
-	Node* tmp = &head;
-	for (int i = 0; i < x; ++i)
-		tmp = tmp->next;
-	node->next = tmp->next;
-	tmp->next = node;
-	if (node->next == nullptr) {
-		tail.next = node;
-	}
-}
-
-void insert_final(int n) {
-	Node* node = make_new_node(n);
-
-	if (tail.next != nullptr) { // 이미 삽인한 노드가 있을 경우에는 마지막 노드도 연결
-		tail.next->next = node;
-	}
-	if (head.next == nullptr)
-		head.next = node;
-	tail.next = node;
-}
-
-void print_10_node() {
-	Node* tmp = head.next;
-
-	for (int i = 0; i < 10; ++i) {
-		cout << tmp->val << " ";
-		tmp = tmp->next;
-		if (tmp == nullptr)
-			break;
-	}
-}
-void print_all_node() {
-	Node* tmp = head.next;
-
-	while (tmp != nullptr) {
-		cout << tmp->val << " ";
-		tmp = tmp->next;
-	}
-	cout << "\n";
-}
-
-int main(int argc, char** argv)
-{
 	for (int tc = 1; tc <= 10; ++tc) {
 		int N;
 		cin >> N;
-		for (int i = 0; i < N; ++i) {
-			int n;
-			cin >> n;
-			insert_final(n);
-		}
-		
-		int command_num;
-		cin >> command_num;
-		for (int i = 0; i < command_num; ++i) {
-			char c;
-			cin >> c;
-			int x, y;
 
-			if (c == 'I') {
+		list<int> v;
+		for (int i = 0; i < N; ++i) {
+			int k;
+			cin >> k;
+			v.push_back(k);
+		}
+
+		int c_num;
+		cin >> c_num;
+		for (int i = 0; i < c_num; ++i) {
+			char com;
+			cin >> com;
+			
+			if (com == 'I') {
+				int x, y;
 				cin >> x >> y;
+				auto it = v.begin();
+				advance(it, x);
 				for (int j = 0; j < y; ++j) {
-					int n;
-					cin >> n;
-					insert_interval(x,n);
-					x++;
+					int k;
+					cin >> k;
+					it = v.insert(it, k);
+					it++;
 				}
 			}
-			else if (c == 'D') {
+			else if (com == 'D') {
+				int x, y;
 				cin >> x >> y;
-				Node* tmp = &head;
-				for (int i = 0; i < x; ++i)
-					tmp = tmp->next;
+
+				auto it = v.begin();
+				advance(it, x);
 				for (int j = 0; j < y; ++j) {
-					Node* dnode = tmp->next;
-					tmp->next = dnode->next;
-					delete dnode;
+					it = v.erase(it);
 				}
 			}
-			else {
-				cin >> y;
-				for (int j = 0; j < y; ++j) {
-					int n;
-					cin >> n;
-					insert_final(n);
+			else if (com == 'A') {
+				int n;
+				cin >> n;
+				for (int j = 0; j < n; ++j) {
+					int k;
+					cin >> k;
+					v.push_back(k);
 				}
 			}
 		}
+
+		auto it = v.begin();
 		cout << "#" << tc << " ";
-		print_10_node();
+		for (int i = 0; i < 10; ++i) {
+			cout << *it << " ";
+			it++;
+		}
 		cout << "\n";
 	}
 }
