70051b0fe820546e1521d87a72944a1839633f3b
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-05-26 01:46:49 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	
+	int a, i;
+	cin >> a >> i;
+
+	cout << a * (i - 1) + 1 << "\n";
+}
