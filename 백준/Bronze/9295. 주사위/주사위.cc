116cad8eefbfabe5786d61d79d6d94b8726e33cf
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-05-25 14:42:19 +0900

    [Bronze IV] Title: 주사위, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+
+	int T;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; ++tc) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << "Case " << tc << ": " << a + b << "\n";
+	}
+}
