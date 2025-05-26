ae1b8d0acf1e4aa11c54c79de0ac07122b5ec594
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-05-26 14:43:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	long long res = ((long long)n)*n;
+	cout << res << "\n" << 2 << "\n";
+}
