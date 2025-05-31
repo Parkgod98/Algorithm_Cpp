d3b1e6504f309d9a1e2c6a768f5bd8871e3dee82
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-05-31 19:22:51 +0900

    [Bronze I] Title: 수열의 변화, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
+using namespace std;
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+	
+	vector<int> v(N);
+	for (int i = 0; i < N - 1; ++i) {
+		cin >> v[i];
+		char tmp;
+		cin >> tmp;
+	}
+	cin >> v[N - 1];
+
+	vector<int> v_new;
+	for (int i = 0; i < K; ++i) {
+		v_new = vector<int>(v.size() - 1);
+
+		for (int j = 0; j < v.size()-1; ++j) {
+			v_new[j] = v[j + 1] - v[j];
+		}
+		v = v_new;
+	}
+
+	for (int i = 0; i < v.size(); ++i) {
+		if (i != v.size() - 1) {
+			cout << v[i] << ",";
+		}
+		else
+			cout << v[i];
+	}
+	
+}
