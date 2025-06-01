3eed3f9a7627ea63f7e57eb702e6a11708fa4707
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-02 00:19:32 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -10,28 +10,12 @@ using namespace std;
 int main() {
 	int X;
 	cin >> X;
-	if (X == 64) {
-		cout << 1 << "\n";
-		return 0;
-	}
 
-	vector<int> v(1, 64);
-	int idx = 0;
-	while (1) {
-		int small_idx = v.size() - 1;
-		v[small_idx] /= 2;
-		v.push_back(v[small_idx]);
-
-		int sum = 0;
-		for (int i = 0; i < v.size() - 1; ++i)
-			sum += v[i];
-		if (sum > X) {
-			v.pop_back();
-		}
-		else if (sum == X) {
-			v.pop_back();
-			cout << v.size() << "\n";
-			break;
-		}
+	int cnt = 0;
+	while (X != 0) {
+		if (X % 2 == 1)
+			++cnt;
+		X /= 2;
 	}
+	cout << cnt;
 }
