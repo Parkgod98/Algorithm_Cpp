58dcd53893c14eb0f72028251f3ca1854559e4a8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-03 22:28:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 976 ms, Memory: 47828 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	map<int, int> mp;
+	for (int i = 0; i < N; ++i) {
+		int n;
+		cin >> n;
+		mp[n]++;
+	}
+
+	int M;
+	cin >> M;
+	
+	for (int i = 0; i < M; ++i) {
+		int n;
+		cin >> n;
+		cout << mp[n] << " ";
+	}
+}
