d8be730d81e2e746253b71e9ffad127ac25ce339
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-03 16:59:51 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	if (N % 2) {
+		cout << "CY";
+	}
+	else
+		cout << "SK";
+}
