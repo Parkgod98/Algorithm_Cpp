620c2fcdeb8e09ef7725a9adf322cbe2091cdba8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-03 16:43:06 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <deque>
+#include <set>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int res = sqrt(N);
+
+	cout << res << "\n";
+}
