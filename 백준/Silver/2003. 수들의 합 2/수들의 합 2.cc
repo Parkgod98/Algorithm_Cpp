37e5d95a100c9aec0d248507467a0113fa257276
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-04 18:51:14 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -14,16 +14,29 @@ int main() {
 		cin >> v[i];
 	}
 
-	vector<int> p_sum(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		p_sum[i] = p_sum[i - 1] + v[i];
-	}
-
+	int i, j;
+	i = 0;
+	j = 1;
+	int sum = v[j];
 	int cnt = 0;
-	for (int i = 0; i <= N; ++i) {
-		for (int j = i + 1; j <= N; ++j) {
-			if (p_sum[j] - p_sum[i] == M)
-				++cnt;
+
+	while (j <= N) {
+		if (sum == M) {
+			sum -= v[i];
+			i++;
+			j++;
+			if (j <= N)
+				sum += v[j];
+			++cnt;
+		}
+		else if (sum < M) {
+			j++;
+			if (j <= N)
+				sum += v[j];
+		}
+		else if (sum > M) {
+			sum -= v[i];
+			i++;
 		}
 	}
 	cout << cnt << "\n";
