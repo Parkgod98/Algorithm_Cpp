6b264538fe32c85385b53ea3e60ade758977c6f9
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-06 00:58:47 +0900

    [Silver IV] Title: 상근이의 여행, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -15,24 +15,11 @@ int main() {
 		int N, M;
 		cin >> N >> M;
 
-		vector<vector<int>> v(N + 1);
-		vector<bool> visited(N + 1);
 		for (int i = 0; i < M; ++i) {
 			int a, b;
 			cin >> a >> b;
-			v[a].push_back(b);
-			v[b].push_back(a);
 		}
 
-		int cnt = 0;
-		for (int i = 1; i <= N; ++i) {
-			for (int &n : v[i]) {
-				if (!visited[n]) {
-					visited[n] = true;
-					++cnt;
-				}
-			}
-		}
-		cout << cnt-1 << "\n";
+		cout << N-1 << "\n";
 	}
 }
