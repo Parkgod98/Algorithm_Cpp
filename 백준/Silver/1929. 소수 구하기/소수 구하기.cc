b640f2145d606beb544f43658431188399b02cec
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-08 14:13:20 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,48 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <map>
-#include <cmath>
-#include <stack>
-#include <queue>
 #include <set>
+#include <queue>
 using namespace std;
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
 
-bool IsPrime(int n)
-{
-	if (n < 2)
-		return false;
-	for (int i = 2; i <= sqrt(n); ++i) {
-		if ( n% i == 0)
-			return false;
-	}
-	return true;
-}
+int main() {
+	int M, N;
+	cin >> M >> N;
 
+	vector<bool> v(N+1,true);
+	v[0] = v[1] = false;
 
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-	vector<bool> v(m + 1, true);
-	v[1] = false;
-	for (int i = 2; i < m + 1; ++i) {
-		if (v[i] == true)
-			for (int j = i*2; j < m + 1; j+=i) {
+	for (int i = 2; i*i <= N; ++i) {
+		if (v[i]) {
+			for (int j = i*2; j <= N; j+=i) {
 				v[j] = false;
 			}
+		}
 	}
-	for (int i = n; i <= m; ++i) {
+
+	for (int i = M; i <= N; ++i) {
 		if (v[i])
 			cout << i << "\n";
 	}
+
 }
