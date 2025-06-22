6a3e058ed4d74cd1f0860c6ed45b6e64139c842a
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-23 01:08:34 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,88 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <set>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
-
-void PrintV(vector<vector<int>>& v)
-{
-	for (vector<int> i : v) {
-		for (int j : i)
-			cout << j << " ";
-		cout << "\n";
-	}
-}
-
-int dy[4] = {-1,0,1,0};
+int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
+int r, c;
 
-void DFS(int y, int x, vector<vector<int>>& v, vector<vector<bool>>& visited, int m, int n)
-{
-	visited[y][x] = 1;
+void bfs(vector<vector<int>>& v, vector<vector<bool>>& visited, int y, int x) {
+	queue<pair<int, int>> q;
+	q.push({ y,x });
 
-	for (int i = 0; i < 4; ++i) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny >= n || nx >= m || ny < 0 || nx < 0)
-			continue;
-		if (!visited[ny][nx] && v[ny][nx] == 1) {
-			DFS(ny, nx, v, visited,m,n);
+	while (!q.empty()) {
+		auto it = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; ++i) {
+			int ny = it.first + dy[i];
+			int nx = it.second + dx[i];
+			if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || v[ny][nx] == 0)
+				continue;
+			q.push({ ny,nx });
+			visited[ny][nx] = true;
 		}
 	}
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t;
-	cin >> t;
+	int tc;
+	cin >> tc;
 
-	for (int c = 0; c < t; ++c) {
-		int m, n, k;
-		cin >> m >> n >> k;
+	for (int i = 0; i < tc; ++i) {
+		cin >> c >> r;
+		vector<vector<int>> v(r, vector<int>(c, 0));
+		vector<vector<bool>> visited(r, vector<bool>(c, false));
 
-		vector<vector<int>> v(n);
-		vector<vector<bool>> visited(n);
-		/* 맵,비지티드 초기화 */
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < m; ++j) {
-				v[i].push_back(0);
-				visited[i].push_back(0);
-			}
-		}
+		int k;
+		cin >> k;
 
-		/* 배추 위치 표기 */
-		for (int i = 0; i < k; ++i) {
-			int y, x;
+		for (int j = 0; j < k; ++j) {
+			int x, y;
 			cin >> x >> y;
 			v[y][x] = 1;
 		}
 
-		int count = 0;
-		for (int rr = 0; rr < n; ++rr) {
-			for (int cc = 0; cc < m; ++cc) {
-				if (!visited[rr][cc] && v[rr][cc] == 1) {
-					DFS(rr, cc, v, visited, m, n);
-					count++;
+		int cnt = 0;
+		for (int i = 0; i < r; ++i) {
+			for (int j = 0; j < c; ++j) {
+				if (!visited[i][j] && v[i][j] == 1) {
+					bfs(v, visited, i, j);
+					++cnt;
 				}
 			}
 		}
-		cout << count << endl;
+		cout << cnt << "\n";
 	}
+	
 }
