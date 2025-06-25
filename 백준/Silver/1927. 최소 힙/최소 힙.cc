f0aa2739678809f13a2b3c21f0fc950999cd163c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-26 00:51:42 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	priority_queue<int> pq;
+
+	for (int i = 0; i < N; ++i) {
+		int x;
+		cin >> x;
+
+		if (x == 0) {
+			if (!pq.empty()) {
+				int n = -pq.top();
+				pq.pop();
+				cout << n << "\n";
+			}
+			else
+				cout << 0 << "\n";
+		}
+		else
+			pq.push(-x);
+	}
+}  
