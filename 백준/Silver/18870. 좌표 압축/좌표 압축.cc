0867b3ad8abc6ce35f2985663e98a8d13223ee3b
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-06-26 13:33:31 +0900

    [Silver II] Title: 좌표 압축, Time: 1716 ms, Memory: 39412 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<int> v(N), v2(N);
+	map<int, int> mp;
+	for (int i = 0; i < N; ++i) {
+		cin >> v[i];
+		v2[i] = v[i];
+	}
+	sort(v2.begin(), v2.end());
+	v2.erase(unique(v2.begin(), v2.end()), v2.end());
+
+	int rank = 0;
+	for (int i = 0; i < v2.size(); ++i)
+		mp[v2[i]] = rank++;
+
+	for (int i = 0; i < N; ++i) {
+		cout << mp[v[i]] << " ";
+	}
+}  
