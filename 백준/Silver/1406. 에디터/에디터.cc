8509834cfdad965d8094b5ea2f102b255cdff370
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-06 01:16:29 +0900

    [Silver II] Title: 에디터, Time: 64 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,81 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
 using namespace std;
-
-#define MX 1000005
-char dat[MX];
-int pre[MX], nxt[MX];
-int unused = 1;
-void traverse();
-
-void insert(int addr, char c) {
-	dat[unused] = c;
-	pre[unused] = addr;
-	nxt[unused] = nxt[addr];
-
-	if(nxt[addr] !=-1)
-		pre[nxt[addr]] = unused;
-	nxt[addr] = unused;
-	++unused;
-}
-
-void erase(int addr) {
-
-	nxt[pre[addr]] = nxt[addr];
-	if(nxt[addr] != -1)
-		pre[nxt[addr]] = pre[addr];
-}
-
-void traverse() {
-	int cur = nxt[0];
-	while(cur != -1) {
-		cout << dat[cur];
-		cur = nxt[cur];
-	}
-}
-
 
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
-	fill(pre, pre + MX, -1);
-	fill(nxt, nxt + MX, -1);
+	list<char> li;
 
 	string s;
 	cin >> s;
-	for(int i = 0; i < s.size(); ++i) {
-		insert(i, s[i]);
+	for (int i = 0; i < s.size(); ++i) {
+		li.push_back(s[i]);
 	}
 
-	int n;
-	cin >> n;
-	cin.ignore();
-	int cur = s.size();
-	int sz = s.size();
-	for(int i = 0; i < n; ++i) {
-		string com;
-		getline(cin, com);
+	int M;
+	cin >> M;
 
-		if(com[0] == 'L') {
-			if(pre[cur] !=-1)
-				cur = pre[cur];
+	auto it = li.end();
+	for (int i = 0; i < M; ++i) {
+		char com;
+		cin >> com;
+		if (com == 'L') {
+			if (it != li.begin())
+				it--;
 		}
-		else if(com[0] == 'D') {
-			if(nxt[cur] !=-1)
-				cur = nxt[cur];
-
+		else if (com == 'D') {
+			if (it != li.end())
+				++it;
 		}
-		else if(com[0] == 'B') {
-			if(cur != 0) {
-				erase(cur);
-				cur = pre[cur];
+		else if (com == 'B') {
+			if (it != li.begin()) {
+				--it;
+				it = li.erase(it);
 			}
 		}
 		else {
-			char tar = com[2];
-			insert(cur, tar);
-			cur = unused - 1;
+			char c;
+			cin >> c;
+			li.insert(it,c);
 		}
 	}
-	traverse();
 
+	while (!li.empty()) {
+		cout << li.front();
+		li.pop_front();
+	}
 }
