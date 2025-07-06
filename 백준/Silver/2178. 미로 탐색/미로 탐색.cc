2c565586bb64e8f1bedcb0c14dbc892919b5ae94
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-07 04:15:40 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,72 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <set>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
 
-void PrintV(vector<vector<int>>& v)
-{
-	for (vector<int> i : v) {
-		for (int j : i)
-			cout << j << " ";
-		cout << "\n";
+void bfs(vector<vector<int>>& mp, vector<vector<int>>& visited, int y, int x) {
+	int cnt = 0;
+	queue<pair<int, int>> q;
+	q.push({ y,x });
+	visited[y][x] = visited[y][x];
+	while (!q.empty()) {
+		pair<int, int> p = q.front();
+		q.pop();
+		cnt++;
+		y = p.first;
+		x = p.second;
+		for (int i = 0; i < 4; ++i) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= mp.size() || nx >= mp[0].size() || visited[ny][nx] || mp[ny][nx] == 0)
+				continue;
+			q.push({ ny,nx });
+			visited[ny][nx] = visited[y][x] +1;
+		}
 	}
 }
 
-int dy[4] = {-1,0,1,0};
-int dx[4] = { 0,1,0,-1 };
 
 int main()
 {
 	int n, m;
 	cin >> n >> m;
 
-	vector<vector<int>> v(n);
-	vector<vector<int>> visited(n);
+	vector<vector<int>> v(n, vector<int>(m,0));
+	vector<vector<int>> visited(n, vector<int>(m, false));
 	for (int i = 0; i < n; ++i) {
 		string s;
 		cin >> s;
-		for (int j = 0; j < m; ++j) {
-			v[i].push_back(s[j] - '0');
-			visited[i].push_back(0);
-		}
+		for (int j = 0; j < s.size(); ++j)
+			v[i][j] = s[j] - '0';
 	}
-
-	int count = 0;
-	queue<pair<int, int>> q;
-	q.push({ 0,0 });
-	visited[0][0] = 1;
-
-	while (q.size()) {
-		pair<int, int> p = q.front();
-		q.pop();
-		int y = p.first;
-		int x = p.second;
-		
-		for (int i = 0; i < 4; ++i) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny >= n || nx >= m || nx < 0 || ny < 0)
-				continue;
-			if (!visited[ny][nx] && v[ny][nx] == 1) {
-				q.push({ ny,nx });
-				visited[ny][nx] = visited[y][x] + 1;
-			}
-		}
-	}
-	cout << visited[n - 1][m - 1] << "\n";
+	bfs(v,visited,0, 0);
+	cout << visited[n - 1][m - 1] +1 << "\n";
 }
