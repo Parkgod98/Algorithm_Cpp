86a0ae45287612cb7b3ea3ad0e1de0b4ddbd26b3
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-08 22:41:49 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,90 +1,62 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
 #include <cmath>
-#include <stack>
+#include <algorithm>
 #include <queue>
-#include <set>
+#include <string>
 using namespace std;
-
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
-
+int N, M, K;
+vector<vector<int>> v;
 int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
 
-void Fill(int x1, int y1, int x2, int y2, vector<vector<int>>& v)
-{
-	for (int i = y1; i < y2; ++i) {
-		for (int j = x1; j < x2; ++j) {
-			v[i][j] = 0;
-		}
-	}
-}
-
-int DFS(int y, int x, vector<vector<int>>& v, vector<vector<bool>>& visited, int m, int n, int &count)
-{
-	visited[y][x] = 1;
-	int ret = 1;
-	for (int i = 0; i < 4; ++i) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= m || nx >= n)
-			continue;
-		if (!visited[ny][nx] && v[ny][nx] == 1)
-			ret += DFS(ny, nx, v, visited, m, n, count);
-	}
-	return ret;
-}
-
-void PrintV(vector<vector<int>>& v)
-{
-	for (vector<int> a : v) {
-		for (int i : a)
-			cout << i << " ";
-		cout << endl;
-	}
-}
-
 int main()
 {
-	int m, n, k;
-	cin >> m >> n >> k;
-
-	vector<vector<int>> v(m);
-	vector<vector<bool>> visited(m);
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			v[i].push_back(1);
-			visited[i].push_back(false);
-		}
-	}
-	for (int i = 0; i < k; ++i) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		Fill(x1, y1, x2, y2, v);
+	cin >> N >> M >> K;
+	v = vector<vector<int>>(N, vector<int>(M,0));
+	for (int i = 0; i < K; ++i) {
+		int a, b, c, d;
+		cin >> b >> a >> d >> c;
+		for (int i = a; i < c; ++i) 
+			for (int j = b; j < d; ++j) 
+				v[i][j] = 1;
 	}
 
-	int count = 0;
-	vector<int> res;
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (!visited[i][j] && v[i][j] == 1) {
-				res.push_back(DFS(i, j, v, visited, m, n, count));
+	vector<vector<bool>> visited(N, vector<bool>(M));
+	vector<int> ans;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			int cnt = 0;
+			if (!visited[i][j] && v[i][j] == 0) {
+				queue<pair<int, int>> q;
+				q.push({ i,j });
+				visited[i][j] = 1;
+
+				while (!q.empty()) {
+					++cnt;
+					auto it = q.front();
+					q.pop();
+
+					int y = it.first;
+					int x = it.second;
+
+					for (int k = 0; k < 4; ++k) {
+						int ny = y + dy[k];
+						int nx = x + dx[k];
+						if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx] || v[ny][nx])
+							continue;
+						q.push({ ny,nx });
+						visited[ny][nx] = 1;
+					}
+				}
+
+				ans.push_back(cnt);
 			}
 		}
 	}
-	sort(res.begin(), res.end());
-	cout << res.size() << "\n";
-	for (int i : res)
-		cout << i << " ";
-
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << "\n";
+	for (int i = 0; i < ans.size(); ++i)
+		cout << ans[i] << " ";
+	cout << "\n";
 }
