404185fdd28bde0b2183249e14fb96437fbed01d
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-08 22:42:01 +0900

    [Silver I] Title: 신입 사원, Time: 476 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,42 +1,38 @@
 #include <iostream>
-#include <cmath>
-#include <string>
 #include <vector>
+#include <cmath>
 #include <algorithm>
-
+#include <queue>
+#include <string>
 using namespace std;
-
-bool compare(pair<int, int> a, pair<int, int> b)
-{
-
-	return a.first < b.first;
-}
 
 int main()
 {
-	int t;
-	cin >> t;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	cin >> T;
 
-	for (int i = 0; i < t; ++i) {
-		int n;
-		cin >> n;
-		vector<pair<int, int>> v(n);
-		for (int j = 0; j < n; ++j) {
+	for (int tc = 1; tc <= T; ++tc) {
+		int N;
+		cin >> N;
+
+		vector<pair<int, int>> v(N);
+		for (int i = 0; i < N; ++i) {
 			int a, b;
 			cin >> a >> b;
-			v[j] = { a,b };
+			v[i] = { a,b };
 		}
-
-		sort(v.begin(), v.end(), compare);
 		
-		int standard = v[0].second;
-		int cnt = 1;
-		for (int j = 1; j < n; ++j) {
-			if (standard > v[j].second) {
-				cnt++;
-				standard = v[j].second;
+		sort(v.begin(), v.end());
+		int mx = 1;
+		int cur = v[0].second;
+		for (int i = 1; i < N; ++i) {
+			if (v[i].second < cur) {
+				++mx;
+				cur = v[i].second;
 			}
 		}
-		cout << cnt << "\n";
+		cout << mx << "\n";
 	}
 }
