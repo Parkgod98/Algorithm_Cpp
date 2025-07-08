2d0be1cddcdae0006c0c8a50ff5a50c948c9a815
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-08 23:34:24 +0900

    [Silver I] Title: 스티커, Time: 268 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+int main(void) {
+	int T;
+	cin >> T;
+
+	for (int tc = 1; tc <= T; ++tc) {
+		int n;
+		cin >> n;
+
+		vector<vector<int>> v(2, vector<int>(n+2));
+		vector<vector<int>> dp(2, vector<int>(n + 2));
+		for (int i = 0; i < n; ++i)
+			cin >> v[0][i];
+		for (int i = 0; i < n; ++i)
+			cin >> v[1][i];
+		dp[0][0] = v[0][0];
+		dp[1][0] = v[1][0];
+		dp[0][1] = dp[1][0] + v[0][1];
+		dp[1][1] = dp[0][0] + v[1][1];
+
+		int mx = max(max(dp[0][0], dp[1][0]), max(dp[0][1], dp[1][1]));
+		for (int i = 2; i < n; ++i) {
+			dp[0][i] = max(dp[1][i - 1] + v[0][i], max(dp[0][i - 2] + v[0][i], dp[1][i - 2] + v[0][i]));
+			dp[1][i] = max(dp[0][i - 1] + v[1][i], max(dp[0][i - 2] + v[1][i], dp[1][i - 2] + v[1][i]));
+			mx = max(mx, max(dp[0][i], dp[1][i]));
+		}
+
+		cout << mx << "\n";
+	}
+}
