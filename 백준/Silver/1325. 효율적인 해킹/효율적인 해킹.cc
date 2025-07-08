04ce697051755c1ce75d952ba9fb6ba72c6d3741
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-08 22:43:36 +0900

    [Silver I] Title: 효율적인 해킹, Time: 4240 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,82 +1,55 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
 #include <cmath>
-#include <stack>
+#include <algorithm>
 #include <queue>
-#include <set>
+#include <string>
 using namespace std;
-typedef long long ll;
+int N, M;
 
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
+int cnt;
+vector<vector<int>> v;
+vector<bool> visited;
 
-int HackCount(vector<vector<int>> & v, int n, vector<int> &visited)
-{
+void DFS(int n) {
+	++cnt;
 	visited[n] = 1;
-	
-	int cnt = 0;
-	for (int i : v[n]) {
-		if (!visited[i])
-			cnt += HackCount(v, i, visited);
+	for (int &i : v[n]) {
+		if (!visited[i]) {
+			DFS(i);
+		}
 	}
-	if (v[n].size() == 0)
-		return 1;
-	return ++cnt;
-}
-
-void InitVisit(vector<int>& visited)
-{
-	for (int i = 0; i < visited.size(); ++i)
-		visited[i] = 0;
-}
-
-bool compare(pair<int, int> a, pair<int, int> b)
-{
-	return a.second > b.second;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n, m;
-	cin >> n >> m;
-
-	vector<vector<int>> v(n + 1);
-	vector<int> visited(n + 1);
-	for (int i = 0; i < m; ++i) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+	v = vector<vector<int>>(N + 1);
+	for (int i = 0; i < M; ++i) {
 		int a, b;
 		cin >> a >> b;
 		v[b].push_back(a);
 	}
 
-	int max_count = 0;
-	vector<pair<int, int>> hack;
-	for (int i = 1; i < v.size(); ++i) {
-		int cnt = HackCount(v, i,visited);
-		hack.push_back({ i,cnt });
-		InitVisit(visited);
+	vector<int> ans;
+	int mx = 0;
+	for (int i = 1; i <= N; ++i) {
+		visited = vector<bool>(N + 1);
+
+		DFS(i);
+		if (mx < cnt) {
+			ans.clear();
+			mx = cnt;
+			ans.push_back(i);
+		}
+		else if (mx == cnt) {
+			ans.push_back(i);
+		}
+		cnt = 0;
 	}
 
-	sort(hack.begin(), hack.end(), compare);
-	vector<int> res;
-	int max_hack = hack[0].second;
-	for (pair<int, int> i : hack) {
-		if (i.second == max_hack)
-			res.push_back(i.first);
-	}
-	sort(res.begin(), res.end());
-	
-	for (int i : res)
+	for (int &i : ans)
 		cout << i << " ";
 }
