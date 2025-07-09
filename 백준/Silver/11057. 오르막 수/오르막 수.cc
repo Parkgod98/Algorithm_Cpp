f5bd7001c6ec96d410f5dc62c70ec533203ffe99
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-09 15:04:21 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -13,11 +13,9 @@ int main()
 		dp[1][i] = 1;
 
 	for (int i = 2; i <= N; ++i) {
-		for (int j = 0; j <= 9; ++j) {
-			for (int k = 0; k <= j; ++k) {
-				dp[i][j] += dp[i - 1][k];
-				dp[i][j] %= MOD;
-			}
+		dp[i][0] = dp[i - 1][0];
+		for (int j = 1; j <= 9; ++j) {
+			dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
 		}
 	}
 
