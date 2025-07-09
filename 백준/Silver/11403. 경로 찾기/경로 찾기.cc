062e6e2ed2b298d5a3e51ff869076d494d046ecd
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-09 13:58:36 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -5,38 +5,30 @@ int N;
 vector<vector<int>> v;
 vector<vector<int>> ans;
 
-void DFS(int start, int cur, vector<int> &visited) {
-	for (int i = 0; i < N; ++i) {
-		if (v[cur][i] == 1) {
-			ans[start][i] = 1;
-			if (!visited[i]) {
-				visited[i] = 1;
-				DFS(start, i, visited);
-			}
-		}
-	}
-}
-
 int main()
 {
 	cin >> N;
 
 	v = vector<vector<int>>(N, vector<int>(N));
-	ans = v;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
 			cin >> v[i][j];
 		}
 	}
 
-	for (int i = 0; i < N; ++i) {
-		vector<int> visited(N);
-		DFS(i,i,visited);
+	for (int k = 0; k < N; ++k) {
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if (v[i][k] && v[k][j]) {
+					v[i][j] = 1;
+				}
+			}
+		}
 	}
 
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j)
-			cout << ans[i][j] << " ";
+			cout << v[i][j] << " ";
 		cout << "\n";
 	}
 }
