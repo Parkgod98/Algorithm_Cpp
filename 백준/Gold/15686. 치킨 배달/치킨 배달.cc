34f8dd66b15c6861ce8d1c38a534df41acdacb47
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-16 12:11:11 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,88 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <cmath>
-#include <stack>
 #include <queue>
+#include <algorithm>
 #include <set>
+#include <map>
+#include <string>
 using namespace std;
-typedef long long ll;
-
-//ios_base::sync_with_stdio(false);
-//cin.tie(NULL);
-//
-//cout << fixed;
-//cout.precision(2);
-// atoi(s.c_str());
-// stoi()
-
-int GetMin(int y, int x, vector<vector<int>>& v, vector<pair<int, int>> chicks, vector<int> &r)
-{
-	int min_distance = 500000;
-	for (int i = 0; i < r.size(); ++i) {
-		int cy = chicks[r[i]].first;
-		int cx = chicks[r[i]].second;
-		min_distance = min(min_distance, abs(x-cx) +abs(y-cy));
-	}
-	return min_distance;
-}
-
-void Comb(int n, int k, int depth, vector<int> &r, vector<vector<int>> &v, vector<pair<int, int>> &chicks, int &res)
-{
-	if (r.size() == k) {
-		for (int i = 0; i < r.size(); ++i) {
-			v[chicks[r[i]].first][chicks[r[i]].second] = 2;
-		}
-		int tmp = 0;
-		for (int i = 0; i < v.size(); ++i) {
-			for (int j = 0; j < v.size(); ++j) {
-				if (v[i][j] == 1)
-					tmp += GetMin(i, j, v, chicks,r);
-				if (tmp >= res)
-					return;
+int N, M;
+vector<vector<int>> v;
+vector<pair<int, int>> chicks,house;
+vector<int> ans;
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+int cnt = 10000000;
+int what = 0;
+void Comb(int depth,int idx) {
+	if (ans.size() == M) {
+		vector<int> visited(house.size(),100000000);
+		for (int i = 0; i < (int)house.size(); ++i) {
+			for (int j = 0; j < (int)ans.size(); ++j) {
+				visited[i] = min(visited[i], abs(house[i].first - chicks[ans[j]].first) + abs(house[i].second - chicks[ans[j]].second));
 			}
 		}
-		res = min(res, tmp);
-		for (int i = 0; i < r.size(); ++i) {
-			v[chicks[r[i]].first][chicks[r[i]].second] = 0;
+		int sum = 0;
+		for (int i = 0; i < (int)house.size(); ++i) {
+			sum += visited[i];
 		}
+		cnt = min(cnt, sum);
 		return;
 	}
-	for (int i = depth; i < chicks.size(); ++i) {
-		r.push_back(i);
-		Comb(chicks.size(), k, i + 1, r, v, chicks,res);
-		r.pop_back();
+
+	for (int i = idx; i < (int)chicks.size(); ++i) {
+		ans.push_back(i);
+		Comb(depth + 1, i + 1);
+		ans.pop_back();
 	}
 }
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> M;
+	v = vector<vector<int>>(N, vector<int>(N));
 
-
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-
-	vector<vector<int>> v(n, vector<int>(n));
-	vector<pair<int,int>> chicks;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
 			cin >> v[i][j];
 			if (v[i][j] == 2) {
 				chicks.push_back({ i,j });
-				v[i][j] = 0;
+			}
+			else if (v[i][j] == 1) {
+				house.push_back({ i,j });
 			}
 		}
 	}
 
-	
-	int res = 5000000;
-	vector<int> r;
-	for (int k = 1; k <= m; ++k) {
-		/* tmp 구하기.*/
-		Comb(chicks.size(), k, 0, r, v, chicks, res);
-	}
-
-	cout << res;
+	Comb(0,0);
+	cout << cnt << "\n";
 }
