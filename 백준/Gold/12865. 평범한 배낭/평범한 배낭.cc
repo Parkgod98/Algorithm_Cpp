03ed9ceafcacd72c729cd45f1e0d15a88f7a3ecc
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-16 17:03:58 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -7,33 +7,20 @@
 #include <string>
 using namespace std;
 int N, K;
-struct Item {
-	int W, V;
-};
 
-vector<Item> v;
 int main() {
 	cin >> N >> K;
-
-	v = vector<Item>(N+1);
-	for (int i = 1; i <= N; ++i) {
-		int w, k;
-		cin >> w >> k;
-		v[i] = { w,k };
-	}
-
-	vector<vector<int>> dp(N+1, vector<int>(K + 1));
-
 	int res = 0;
-	for (int i = 1; i <= N; ++i) {
-		for (int j = 1; j <= K; ++j) {
-			if (j >= v[i].W) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i].W] + v[i].V);
-			}
-			else
-				dp[i][j] = dp[i - 1][j];
-			res = max(dp[i][j], res);
+
+	vector<int> dp(K + 1);
+	for (int i = 0; i < N; ++i) {
+		int w, v;
+		cin >> w >> v;
+		for (int j = K; j >= w; --j) {
+			dp[j] = max(dp[j], dp[j - w] + v);
+			res = max(res, dp[j]);
 		}
 	}
+
 	cout << res << "\n";
 }
