21c33cb41d653e65746cde44354f99f1ea9fe54e
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-17 09:29:13 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,44 +1,41 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <string>
 using namespace std;
-
-int dy[4] = { -1,0,1,0};
-int dx[4] = { 0,1,0,-1};
-#define SZ 100001
-#define INF 10000000
-vector<int> visited(SZ, INF);
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	int s, e;
-	cin >> s >> e;
+int N, K;
+#define INF 2000000
+int main() {
+	cin >> N >> K;
 
 	queue<int> q;
-	q.push(s);
-	visited[s] = 1;
+	q.push(N);
+	vector<int> visited(100001,2000000);
+	visited[N] = 1;
 
 	while (!q.empty()) {
 		int n = q.front();
 		q.pop();
-		if (n == e) {
-			cout << visited[n] -1 << "\n";
+
+		if (n == K) {
+			cout << visited[n] - 1 << "\n";
 			break;
 		}
 
-		if (n * 2 <= SZ && visited[n * 2] == INF) {
+		if (n * 2 <= 100000 && visited[n * 2] == INF) {
 			q.push(n * 2);
 			visited[n * 2] = min(visited[n * 2], visited[n]);
 		}
 		if (n - 1 >= 0 && visited[n - 1] == INF) {
 			q.push(n - 1);
-			visited[n - 1] = min(visited[n-1],visited[n] + 1);
+			visited[n - 1] = min(visited[n - 1], visited[n] + 1);
 		}
-		if (n + 1 <= SZ && visited[n + 1] == INF) {
+		if (n + 1 <= 100000 && visited[n + 1] == INF) {
 			q.push(n + 1);
-			visited[n + 1] = min(visited[n+1], visited[n] + 1);
+			visited[n + 1] = min(visited[n + 1], visited[n] + 1);
 		}
-
 	}
 }
