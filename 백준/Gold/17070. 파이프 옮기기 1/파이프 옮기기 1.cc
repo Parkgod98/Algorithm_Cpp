57db2739f05ef17c862d39aed3e7951f5fd83006
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-22 10:10:36 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -5,52 +5,35 @@
 #include <cmath>
 #include <queue>
 using namespace std;
-int N,dir;
+int N;
 vector<vector<int>> v;
-int cnt;
 
-bool Check(int y, int x) {
-
-	return (y >= 1 && y <= N && x >= 1 && x <= N) && v[y][x] != 1;
-}
-
-void DFS(int y1, int x1, int y2, int x2, int dir) {
-	if (y2 == N && x2 == N) {
-		++cnt;
-		return;
-	}
-
-	if (dir == 0) { // 가로
-		if (Check(y1, x1 + 1) && Check(y2,x2+1)) 
-			DFS(y1, x1 + 1, y2, x2 + 1, 0);
-		if (Check(y1, x1 + 1) && Check(y2 + 1, x2 + 1) && Check(y2+1,x2) && Check(y2,x2+1))
-			DFS(y1, x1 + 1, y2 + 1, x2 + 1, 1);
-	}
-	else if (dir == 1) { // 대각
-		if (Check(y1 + 1, x1+1) && Check(y2, x2+1))
-			DFS(y1 + 1, x1+1, y2, x2+1, 0);
-		if (Check(y1 + 1, x1+1) && Check(y2 + 1, x2))
-			DFS(y1 + 1, x1+1, y2 + 1, x2, 2);
-		if (Check(y1 + 1, x1+1) && Check(y2 + 1, x2 + 1) && Check(y2 + 1, x2) && Check(y2, x2 + 1))
-			DFS(y1 + 1, x1+1, y2 + 1, x2 + 1, 1);
-	}
-	else { // 세로
-		if (Check(y1+1, x1) && Check(y2+1, x2))
-			DFS(y1+1, x1, y2+1, x2, 2);
-		if (Check(y1+1, x1) && Check(y2 + 1, x2 + 1) && Check(y2 + 1, x2) && Check(y2, x2 + 1))
-			DFS(y1+1, x1, y2 + 1, x2 + 1, 1);
-	}
-}
 int main() {
 	cin >> N;
 	v = vector<vector<int>>(N + 1, vector<int>(N + 1));
-
+	vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(N + 1, vector<int>(3)));
+	dp[1][2][0] = 1;
 	for (int i = 1; i <= N; ++i) {
 		for (int j = 1; j <= N; ++j)
 			cin >> v[i][j];
 	}
 
-	DFS(1, 1, 1, 2, dir);
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 2; j <= N; ++j) {
+			if (i == 1 && j == 2)
+				continue;
+			if (v[i][j] == 0) {
+				dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][1];
+				dp[i][j][2] = dp[i - 1][j][2] + dp[i - 1][j][1];
+				if (v[i][j-1] != 1 && v[i-1][j] != 1)
+					dp[i][j][1] = dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2] + dp[i - 1][j - 1][0];
+			}
+			
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i < 3; ++i)
+		sum += dp[N][N][i];
 
-	cout << cnt << "\n";
+	cout << sum << "\n";
 }
