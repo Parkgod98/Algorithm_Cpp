babffc10d48fa468f490bfbddc00e718572f7a29
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-23 10:57:40 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+using namespace std;
+int N, M;
+vector<int> dp(101, 0);
+vector<int> skip(101);
+vector<int> visited(101, 99999999);
+int mn = 99999999;
+void DFS(int cur, int cnt) {
+	if (visited[cur] > cnt)
+		visited[cur] = cnt;
+	else
+		return;
+	if (cur == 100) {
+		mn = min(mn, cnt);
+		return;
+	}
+	if (skip[cur] != 0) {
+			DFS(skip[cur],cnt);
+			return;
+	}
+	for (int i = 1; i <= 6; ++i) {
+		if(cur + i <= 100)
+			DFS(cur + i,cnt+1);
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	
+	for (int i = 0; i < N + M; ++i) {
+		int a, b;
+		cin >> a >> b;
+		skip[a] = b;
+	}
+
+	DFS(1,1);
+	cout << mn -1 << "\n";
+}
