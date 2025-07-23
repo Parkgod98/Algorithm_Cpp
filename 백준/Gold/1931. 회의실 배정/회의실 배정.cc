9246ec2fb52204b0eec362f15c0c43c13bb4c283
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-23 16:16:29 +0900

    [Gold V] Title: 회의실 배정, Time: 80 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.cc
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
-#include <set>
-#include <map>
 #include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
 using namespace std;
 int N;
 
 int main() {
 	cin >> N;
-
 	vector<pair<int, int>> v(N);
 	for (int i = 0; i < N; ++i) {
-		int a, b;
-		cin >> a >> b;
-		v[i] = { b,a };
+		int s, t;
+		cin >> s >> t;
+		v[i] = { t,s };
 	}
-	sort(v.begin(), v.end());
 
+	sort(v.begin(), v.end());
 	int cnt = 0;
-	int time = 0;
+	int end_time = 0;
 	for (int i = 0; i < N; ++i) {
-		if (time <= v[i].second) {
+		int cur = v[i].second;
+		if (cur >= end_time) {
 			++cnt;
-			time = v[i].first;
+			end_time = v[i].first;
 		}
 	}
 
