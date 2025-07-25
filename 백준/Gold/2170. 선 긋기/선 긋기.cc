548d940c44c3dee77035fa70310be6744a227cfd
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-25 13:34:39 +0900

    [Gold V] Title: 선 긋기, Time: 344 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+using namespace std;
+int N;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	vector<pair<int, int>> v(N);
+	for (int i = 0; i < N; ++i) {
+		int a, b;
+		cin >> a >> b;
+		if (a > b)
+			swap(a, b);
+		v[i] = { a,b };
+	}
+
+	sort(v.begin(), v.end());
+
+	int start_time = v[0].first;
+	int end_time = v[0].second;
+
+	int res = 0;
+	for (int i = 1; i < N; ++i) {
+		if (v[i].first <= end_time) {
+			if (end_time <= v[i].second)
+				end_time = v[i].second;
+		}
+		else {
+			res += end_time - start_time;
+			start_time = v[i].first;
+			end_time = v[i].second;
+		}
+	}
+	res += (end_time - start_time);
+
+	cout << res << "\n";
+}
