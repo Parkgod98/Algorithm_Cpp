7717005078cdbe1e54502fbb979fcf80451ded61
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-07-25 16:21:10 +0900

    [Gold V] Title: 퇴사 2, Time: 180 ms, Memory: 7880 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -9,34 +9,20 @@
 using namespace std;
 int N;
 
-struct Cons {
-	int day, money;
-};
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
+
 	cin >> N;
-
-	vector<Cons> v(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		int a, b;
-		cin >> a >> b;
-		v[i] = { a,b };
-	}
-
 	vector<int> dp(N + 2); // 2차원이 될 수도 있어. 아직은 잘 몰라. DP라는것까지만.
-	int mx = 0;
 	for (int i = 1; i <= N; ++i) {
-		dp[i] = max(dp[i - 1], dp[i]);
-		if (i + v[i].day <= N+1) {
-			dp[i + v[i].day] = max(dp[i + v[i].day], dp[i] + v[i].money);
-			mx = max(mx, dp[i+v[i].day]);
+		int day, money;
+		cin >> day >> money;
 
-		}
-		mx = max(mx, dp[i]);
+		if (i + day <= N + 1)
+			dp[i + day] = max(dp[i + day], dp[i] + money);
+		dp[i + 1] = max(dp[i], dp[i + 1]);
+		
 	}
-
-	cout << mx << "\n";
-
+	cout << dp[N + 1] << "\n";
 }
