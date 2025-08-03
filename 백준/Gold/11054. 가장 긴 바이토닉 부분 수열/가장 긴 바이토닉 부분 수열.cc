7f71b100d1af484cde157a243e7b58941d426e05
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-08-03 17:27:30 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -20,33 +20,20 @@ int main() {
 		}
 	}
 
-	reverse(v.begin(), v.end());
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < i; ++j) {
+	for (int i = N-1; i >=0; --i) {
+		for (int j = N-1; j >i; --j) {
 			if (v[i] > v[j])
 				dp2[i] = max(dp2[i], dp2[j] + 1);
 		}
 	}
-	reverse(dp2.begin(), dp2.end());
 
-	int idx = -1;
 	int mx = -1;
 	for (int i = 0; i < N; ++i) {
 		if (mx < dp[i] + dp2[i]) {
 			mx = dp[i] + dp2[i];
-			idx = i;
 		}
 	}
 
-	//for (int &n : dp)
-	//	cout << n << " ";
-	//cout << "\n";
-
-	//for (int &n : dp2)
-	//	cout << n << " ";
-	//cout << "\n";
-
 	int res = mx - 1;
 	cout << res << "\n";
-
 }
