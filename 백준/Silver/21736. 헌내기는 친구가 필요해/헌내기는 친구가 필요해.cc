2793a7a8e529ad4ed12bed40849d7e6bedb40687
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-08-14 13:59:52 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 24 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -1,59 +1,61 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <set>
-#include <map>
-#include <list>
 #include <queue>
-#include <stack>
+#include <algorithm>
+#include <map>
 #include <cmath>
+#include <string>
 using namespace std;
-int N,M;
+int N, M;
 vector<vector<char>> v;
 vector<vector<bool>> visited;
 int dy[4] = { -1,0,1,0 };
 int dx[4] = { 0,1,0,-1 };
+
+struct Point {
+	int y, x;
+};
+Point doy;
 int main() {
 	cin >> N >> M;
 	v = vector<vector<char>>(N, vector<char>(M));
-	visited = vector<vector<bool>>(N, vector<bool>(M));
-	pair<int, int> start;
-	for (int i = 0; i < N; ++i)
+	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < M; ++j) {
 			cin >> v[i][j];
-			if (v[i][j] == 'I') {
-				start = { i,j };
-				visited[i][j] = 1;
-			}
+			if (v[i][j] == 'I')
+				doy = { i,j };
 		}
+	}
 
-	queue<pair<int, int>> q;
-	q.push(start);
+	visited = vector<vector<bool>>(N, vector<bool>(M));
+	queue<Point> q;
+	q.push(doy);
+	visited[doy.y][doy.x] = 1;
 
 	int cnt = 0;
 	while (!q.empty()) {
 		auto it = q.front();
 		q.pop();
-		int y = it.first;
-		int x = it.second;
+
+		int y = it.y;
+		int x = it.x;
 		if (v[y][x] == 'P')
 			++cnt;
-		
 		for (int i = 0; i < 4; ++i) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx])
-				continue;
-			if (v[ny][nx] == 'X')
+			if (ny < 0 || ny >= N || nx < 0 || nx >= M || visited[ny][nx] || v[ny][nx] == 'X')
 				continue;
 			q.push({ ny,nx });
 			visited[ny][nx] = 1;
 		}
 	}
-
 	if (cnt == 0)
 		cout << "TT" << "\n";
 	else
 		cout << cnt << "\n";
+
+
 }
+
+
