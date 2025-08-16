937f3f78ab53cf40825733b5bb37ec249926c5b0
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-08-16 17:55:59 +0900

    [D3] Title: 이진수 표현, Time: 554 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <queue>
-#include <vector>
-#include <algorithm>
 using namespace std;
 int N, M;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 	int T;
 	cin >> T;
 
 	for (int tc = 1; tc <= T; ++tc) {
 		cin >> N >> M;
-		int mask = (1 << N) - 1;
-		if ((M&mask) == mask) {
+		if ((M&((1 << N) - 1)) == ((1 << N) - 1)) 
 			cout << "#" << tc << " " << "ON" << "\n";
-		}
 		else
 			cout << "#" << tc << " " << "OFF" << "\n";
 	}
