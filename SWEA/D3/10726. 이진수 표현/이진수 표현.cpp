8f6829deb926279af45d0f477227c29de88d2032
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-08-16 17:52:49 +0900

    [D3] Title: 이진수 표현, Time: 554 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.cpp
@@ -1,62 +1,21 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// float b, c;
-// double d, e, f;
-// char g;
-// char var[256];
-// long long AB;
-// cin >> a;                            // int 변수 1개 입력받는 예제
-// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
-// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
-// cin >> g;                            // char 변수 1개 입력받는 예제
-// cin >> var;                          // 문자열 1개 입력받는 예제
-// cin >> AB;                           // long long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// float b = 1.0, c = 2.0;               
-// double d = 3.0, e = 0.0; f = 1.0;
-// char g = 'b';
-// char var[256] = "ABCDEFG";
-// long long AB = 12345678901234567L;
-// cout << a;                           // int 변수 1개 출력하는 예제
-// cout << b << " " << c;               // float 변수 2개 출력하는 예제
-// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
-// cout << g;                           // char 변수 1개 출력하는 예제
-// cout << var;                         // 문자열 1개 출력하는 예제
-// cout << AB;                          // long long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-#include<iostream>
-
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int N, M;
 
-bool IsOkay(int &n, int& m)
-{
-	int mask = (1 << n) - 1;
-	m &= mask;
-	if (m == mask)
-		return true;
-	return false;
-}
+int main() {
+	int T;
+	cin >> T;
 
-int main(int argc, char** argv)
-{
-	int t;
-	cin >> t;
-
-	for (int i = 1; i <= t; ++i) {
-		int n, m;
-		cin >> n >> m;
-		string result = "OFF";
-		if (IsOkay(n, m))
-			result = "ON";
-		cout << "#" << i << " " << result << "\n";
-
-
+	for (int tc = 1; tc <= T; ++tc) {
+		cin >> N >> M;
+		int mask = (1 << N) - 1;
+		if ((M&mask) == mask) {
+			cout << "#" << tc << " " << "ON" << "\n";
+		}
+		else
+			cout << "#" << tc << " " << "OFF" << "\n";
 	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
