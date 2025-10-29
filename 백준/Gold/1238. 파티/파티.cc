a33333320f12855108824538e47c554aa3c34326
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-10-30 08:10:10 +0900

    [Gold III] Title: 파티, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -5,7 +5,7 @@
 #include <map>
 #include <queue>
 using namespace std;
-#define INF 9999999999
+#define INF 999999999
 struct Edge {
 	int nxt;
 	int cost;
@@ -16,7 +16,7 @@ struct Edge {
 	}
 };
 int N, M, X;
-vector<vector<Edge>> v;
+vector<vector<Edge>> v, v2;
 
 vector<int> distra(int start) {
 	priority_queue<Edge> pq;
@@ -48,23 +48,54 @@ vector<int> distra(int start) {
 	return dist;
 }
 
+vector<int> distra2(int start) {
+	priority_queue<Edge> pq;
+	vector<int> dist(N + 1, INF);
+
+	pq.push({ start,0 });
+	dist[start] = 0;
+
+	while (!pq.empty()) {
+		auto it = pq.top();
+		int cur = it.nxt;
+		int cost = it.cost;
+		pq.pop();
+
+		if (dist[cur] < cost)
+			continue;
+
+		for (auto iit : v2[cur]) {
+			int nxt = iit.nxt;
+			int nxt_cost = iit.cost;
+
+			if (cost + nxt_cost >= dist[nxt])
+				continue;
+			pq.push({ nxt,cost + nxt_cost });
+			dist[nxt] = cost + nxt_cost;
+		}
+	}
+
+	return dist;
+}
+
 int main() {
 	cin >> N >> M >> X;
-	v = vector<vector<Edge>>(N + 1);
+	v = v2 = vector<vector<Edge>>(N + 1);
 	for (int i = 0; i < M; ++i) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		v[a].push_back({ b,c });
+		v2[b].push_back({ a,c });
 	}
 
 	vector<int> x_dist = distra(X);
+	vector<int> x_dist2 = distra2(X);
 	int mx = 0;
 	for (int i = 1; i <= N; ++i) {
 		if (i == X)
 			continue;
-		vector<int> dist = distra(i);
-		if (mx < dist[X] + x_dist[i])
-			mx = dist[X] + x_dist[i];
+		if (mx < x_dist[i] + x_dist2[i])
+			mx = x_dist[i] + x_dist2[i];
 	}
 	cout << mx << "\n";
 
