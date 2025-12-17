1308c33a98bc7cd1506e23b4e6855f347a7e3cff
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-12-17 23:56:31 +0900

    [Gold III] Title: 순회강연, Time: 44 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cctype>
+#include <set>
+#include <queue>
+#include <map>
+#include <cmath>
+using namespace std;
+
+int N;
+int d, w;
+vector<pair<int, int>> v;
+
+int day[10001];
+bool cmp(pair<int, int> &a, pair<int, int> &b) {
+	if (a.second != b.second)
+		return a.second > b.second;
+
+	return a.first < b.first;
+}
+int main(){
+	
+	cin >> N;
+	v = vector<pair<int, int>>(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> d >> w;
+		v[i] = { w,d };
+	}
+
+	sort(v.begin(), v.end(),cmp);
+
+	for (int i = 0; i < N; ++i) {
+
+		for (int j = v[i].first; j > 0; --j) {
+
+			if (day[j] == 0) {
+				day[j] = v[i].second;
+				break;
+			}
+		}
+	}
+
+	int sum = 0;
+	for (int i = 1; i <= 10000; ++i)
+		sum += day[i];
+
+	cout << sum << "\n";
+}
