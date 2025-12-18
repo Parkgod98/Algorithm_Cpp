4ef62cdf9fad03de22464957e511def9401b2c9e
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2025-12-18 12:01:06 +0900

    [D3] Title: 0/1 Knapsack, Time: 43 ms, Memory: 6,720 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.cpp
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <cctype>
+#include <set>
+#include <queue>
+#include <map>
+#include <cmath>
 using namespace std;
+
 int N, K;
 struct Item {
 	int v, c;
@@ -12,14 +17,12 @@ vector<Item> v;
 vector<vector<int>> dp;
 
 int recur(int idx, int volume) {
-
 	if (volume > K)
 		return -1000000000;
 
-	if (idx >= N + 1) {
+	if (idx >= N + 1)
 		return 0;
-	}
-	
+
 	if (dp[idx][volume] != -1)
 		return dp[idx][volume];
 
@@ -28,20 +31,22 @@ int recur(int idx, int volume) {
 
 	dp[idx][volume] = max(a, b);
 
-	return max(a, b);
+	return dp[idx][volume];
 }
-int main() {
+int main(){
 	int T;
 	cin >> T;
 
 	for (int tc = 1; tc <= T; ++tc) {
 		cin >> N >> K;
 		v = vector<Item>(N + 1);
+		
 		for (int i = 1; i <= N; ++i) {
 			int vi, c;
 			cin >> vi >> c;
 			v[i] = { vi,c };
 		}
+
 		dp = vector<vector<int>>(N + 1, vector<int>(K + 1, -1));
 		int res = recur(1, 0);
 
