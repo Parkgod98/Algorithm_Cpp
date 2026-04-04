04622d7145b3de3bd824ac87a399592d356da6bb
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-04-05 00:29:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 84 ms, Memory: 10928 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-#include <set>
-#include <map>
-#include <list>
-#include <queue>
-#include <stack>
-#include <cmath>
 using namespace std;
+
 int N, M, R;
-
-vector<vector<int>> v;
-vector<int> visited;
-int depth = 1;
+vector<int> v[100002];
+int visited[100002];
+int cnt = 1;
 void DFS(int cur) {
-	if (visited[cur])
-		return;
-
-	visited[cur] = depth++;
-	for (int &i : v[cur]) {
-		if (!visited[i])
-			DFS(i);
+	visited[cur] = cnt++;
+	for (int &n : v[cur]) {
+		if (!visited[n]) {
+			DFS(n);
+		}
 	}
 }
 int main() {
@@ -30,24 +21,20 @@ int main() {
 	
 	cin >> N >> M >> R;
 
-	v = vector<vector<int>>(N + 1);
-	visited = vector<int>(N + 1);
 
 	for (int i = 0; i < M; ++i) {
 		int a, b;
 		cin >> a >> b;
-
 		v[a].push_back(b);
 		v[b].push_back(a);
 	}
+
 	for (int i = 1; i <= N; ++i)
 		sort(v[i].begin(), v[i].end());
+
 	DFS(R);
 
 	for (int i = 1; i <= N; ++i) {
-		if (visited[i] >= 1)
-			cout << visited[i] << "\n";
-		else
-			cout << 0 << "\n";
+		cout << visited[i] << "\n";
 	}
-}  
+}
