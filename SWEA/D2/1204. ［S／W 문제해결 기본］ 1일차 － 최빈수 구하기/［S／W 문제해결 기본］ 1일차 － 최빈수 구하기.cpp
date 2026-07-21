e89eef857bea884ea1e48888f50fc28d080fc0dd
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-07-21 13:59:53 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 5 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.cpp
@@ -1,38 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+#define SZ 102
 
-int sc[101];
-int T;
+int arr[SZ];
+int T, N;
 
-int main(int argc, char** argv)
-{
+int main() {
+
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	cin >> T;
 
 	for (int tc = 1; tc <= T; ++tc) {
-		int t;
-		cin >> t;
+		cin >> N;
 
-		for (int i = 0; i <= 100; ++i)
-			sc[i] = 0;
 		for (int i = 0; i < 1000; ++i) {
 			int n;
 			cin >> n;
-			sc[n]++;
+			arr[n]++;
 		}
 
-		int mx = -1;
-		int res = -1;
-		for (int i = 0; i <= 100; ++i) {
-			if (mx <= sc[i]) {
-				mx = sc[i];
+		int res = 0;
+		int k = 0;
+		for (int i = 100; i >= 0; --i) {
+			if (k < arr[i]) {
+				k = arr[i];
 				res = i;
 			}
+			arr[i] = 0;
 		}
 
 		cout << "#" << tc << " " << res << "\n";
-	}
 
+	}
 }
