e2df4c1cb94c75c8c040075476c97a892e796d29
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-07-29 10:34:43 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+int solution(int angle) {
+    if (angle < 90)
+        return 1;
+    else if(angle > 90 && angle < 180)
+        return 3;
+    else if(angle == 90)
+        return 2;
+    else
+        return 4;
+}
