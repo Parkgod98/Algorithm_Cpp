0e0e1925d4368e6db134b82fb9df38d17ad86bb8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-05 15:08:45 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> names) {
+    vector<string> answer;
+    
+    int sz = names.size();
+    for (int i = 0; i < sz; i += 5)
+        answer.push_back(names[i]);
+    return answer;
+}
