652067965f36355324516ab9460be2a691aecbba
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-13 18:35:24 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.cpp
@@ -1,19 +1,25 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 vector<string> solution(vector<string> str_list) {
-     vector<string> answer;
-     int l_idx = find(str_list.begin(), str_list.end(), "l") - str_list.begin();
-     int r_idx = find(str_list.begin(), str_list.end(), "r") - str_list.begin();
-     if (l_idx == string::npos && r_idx == string::npos)
-          return answer;
-     if (l_idx < r_idx) {
-          return vector<string>(str_list.begin(), str_list.begin() + l_idx);
-     }
-     else if (l_idx > r_idx) {
-          return vector<string>(str_list.begin() + r_idx + 1, str_list.end());
-     }
-     return answer;
+    vector<string> answer;
+    
+    int sz = str_list.size();
+    
+    for (int i = 0; i < sz; ++i){
+        if(str_list[i] == "l"){
+            
+            for (int k = 0; k < i; ++k)
+                answer.push_back(str_list[k]);
+            break;
+        }
+        else if(str_list[i] == "r"){
+            for (int k = i+1; k < sz; ++k)
+                answer.push_back(str_list[k]);
+            break;
+        }
+    }
+    return answer;
 }
