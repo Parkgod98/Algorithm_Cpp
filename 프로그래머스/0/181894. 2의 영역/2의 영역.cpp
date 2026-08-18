b8fc31dc6485ccfd394ca12afc361e80f466639c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-18 13:24:32 +0900

    [level 0] Title: 2의 영역, Time: 18.47 ms, Memory: 23.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -5,27 +5,22 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
-    int s,e;
-    s = 0;
-    e = arr.size();
+    
+    int s = -1;
+    int e = -1;
     for (int i = 0; i < arr.size(); ++i){
-        if (arr[i] == 2){
+        if(s == -1 && arr[i] == 2)
             s = i;
-            break;
-        }
-    }
-    for (int i = e -1; i >=0; --i){
-        if (arr[i] == 2){
+        if(s!= -1 && arr[i] == 2){
             e = i;
-            break;
         }
     }
-    if (e!=arr.size()){
-        for (int i = s; i <=e; ++i)
-            answer.push_back(arr[i]);
-    }
-    else{
+    
+    if(s == -1)
         answer.push_back(-1);
+    else{
+        for (s; s <= e; ++s)
+            answer.push_back(arr[s]);
     }
     return answer;
 }
