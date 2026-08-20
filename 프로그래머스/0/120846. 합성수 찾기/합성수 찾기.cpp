1ef59aa19da29ab92c6458dc2ff45a4f41984ae5
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-20 13:45:22 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -4,24 +4,16 @@
 using namespace std;
 
 bool CountYak(int n){
-    
-    int cnt = 0;
-    for (int i = 1; i*i <= n; ++i){
-        if(n%i == 0 && i*i != n){
-            cnt += 2;
-        }
-        else if(i*i == n){
-            cnt += 1;
-        }
+    for (int i = 2; i*i <= n; ++i){
+        if(n%i == 0)
+            return true;
     }
-    if(cnt >= 3)
-        return true;
     return false;
 }
 
 int solution(int n) {
     int cnt = 0;
-    for (int i = 1; i <= n; ++i){
+    for (int i = 4; i <= n; ++i){
         if(CountYak(i))
             ++cnt;
     }
