9f3b3f3d1a340083142b25feb2f07450c202cde6
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 10:55:03 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -3,36 +3,19 @@
 
 using namespace std;
 
-bool IsPrime(int n){
-    
-    for (int i = 2; i *i <= n; ++i){
-        if(n%i == 0)
-            return false;
-    }
-    return true;
-}
-
-int GetNextPrime(int n){
-    
-    while(!IsPrime(n)){
-        ++n;
-    }
-    return n;
-}
-
 vector<int> solution(int n) {
     vector<int> ans;
     
-    int prime = 2;
-    while(n != 1){
-        bool f = false;
-        while(n%prime == 0){
-            n/=prime;
-            f = true;
+    for (int i = 2; i * i <= n; ++i){
+        if(n%i == 0){
+            ans.push_back(i);
         }
-        if(f)
-            ans.push_back(prime);
-        prime = GetNextPrime(prime+1);
+        
+        while(n%i == 0)
+            n/= i;
     }
+    
+    if(n > 1)
+        ans.push_back(n);
     return ans;
 }
