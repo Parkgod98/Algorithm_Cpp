dd315c3a400dfc6b7f220127790df1ce247eba8b
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 11:05:27 +0900

    [level 0] Title: 배열 만들기 4, Time: 0.97 ms, Memory: 6.84 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -1,28 +1,28 @@
 #include <string>
 #include <vector>
-#include <iostream>
-using namespace std;
 
-void PrintV(vector<int> v){
-    for (int i : v)
-        cout << i << " ";
-    cout << "\n";
-}
+using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> stk;
     
     int i = 0;
-    while(i < arr.size()){
-        if(stk.size() == 0)
-            stk.push_back(arr[i++]);
-        else {
-            if(stk[stk.size()-1] < arr[i])
-                stk.push_back(arr[i++]);
-            else
-                stk.pop_back();
+    int sz = arr.size();
+    
+    while(i < sz){
+        if(stk.empty()){
+            stk.push_back(arr[i]);
+            ++i;
         }
-        
+        else{
+            if(stk[stk.size()-1] < arr[i]){
+                stk.push_back(arr[i]);
+                ++i;
+            }
+            else{
+                stk.pop_back();
+            }
+        }
     }
     return stk;
 }
