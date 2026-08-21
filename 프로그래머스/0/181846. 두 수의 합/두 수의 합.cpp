458acff4b823f2be9700fa4146618979ace7d0fe
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 12:56:08 +0900

    [level 0] Title: 두 수의 합, Time: 0.43 ms, Memory: 5.01 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
@@ -1,60 +1,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 string solution(string a, string b) {
-	string answer = "";
-	int a_len = a.length();
-	int b_len = b.length();
-	int carry = 0;
-	if (a_len == b_len) {
-		
-		for (int i = a_len - 1; i >= 0; --i) {
-			int n = (a[i] - '0') + (b[i] - '0') + carry;
-			carry = 0;
-			if (n > 9)
-				carry += 1;
-			answer = to_string(n % 10) + answer;
-		}
-	}
-	else if (a_len > b_len) {
-		int diff = a_len - b_len;
-		for (int i = b_len - 1; i >= 0; --i) {
-			int n = (a[i+diff] - '0') + (b[i] - '0') + carry;
-			carry = 0;
-			if (n > 9)
-				carry += 1;
-			answer = to_string(n % 10) + answer;
-		}
-		for (int i = diff-1; i >= 0; --i) {
-			int n = (a[i] - '0') + carry;
-			carry = 0;
-			if (n > 9)
-				carry += 1;
-			answer = to_string(n % 10) + answer;
-		}
-		
-	}
-	else {
-		int diff = b_len - a_len;
-		for (int i = a_len - 1; i >= 0; --i) {
-			int n = (a[i] - '0') + (b[i + diff] - '0') + carry;
-			carry = 0;
-			if (n > 9)
-				carry += 1;
-			answer = to_string(n % 10) + answer;
-		}
-		for (int i = diff - 1; i >= 0; --i) {
-			int n = (b[i] - '0') + carry;
-			carry = 0;
-			if (n > 9)
-				carry += 1;
-			answer = to_string(n % 10) + answer;
-		}
-	}
-	if(carry !=0)
-		answer = to_string(carry) + answer;
-	return answer;
+    string ans = "";
+    
+    int aidx = a.size() -1;
+    int bidx = b.size() - 1;
+    int carry = 0;
+    while(aidx >= 0 && bidx >= 0){
+        int sum = (a[aidx--] - '0') + (b[bidx--] - '0') + carry;
+        carry = sum/10;
+        ans += (sum%10 + '0');
+    }
+    while(aidx >= 0){
+        int sum = (a[aidx--] - '0') + carry;
+        carry = sum/10;
+        ans += (sum%10 + '0');
+    }
+    while(bidx >= 0){
+        int sum = (b[bidx--] - '0') + carry;
+        carry = sum/10;
+        ans += (sum%10 + '0');
+    }
+    
+    if(carry)
+        ans += (carry + '0');
+    
+    reverse(ans.begin(),ans.end());
+    return ans;
 }
