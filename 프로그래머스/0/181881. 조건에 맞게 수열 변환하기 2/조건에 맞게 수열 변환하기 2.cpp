13ce5b63463183068db3070bda93e3db121bdd75
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 10:27:51 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 24.49 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.cpp
@@ -4,21 +4,25 @@
 using namespace std;
 
 int solution(vector<int> arr) {
-    int answer = 0;
-    bool IsTrue = 0;
-    do{
-        IsTrue = false;
-        answer++;
-        for (int i = 0; i < arr.size(); ++i){
-            if (arr[i]%2 ==0 && arr[i] >=50){
-                arr[i]/=2;
-                IsTrue = true;
+    int x = 0;    
+    bool diff = false;
+    int sz = arr.size();
+    
+    while(1){
+        for (int i = 0; i < sz; ++i){
+            if(arr[i] >=50 && arr[i]%2 == 0){
+                arr[i] /=2;
+                diff = true;
             }
-            else if (arr[i] < 50 && arr[i]%2 == 1){
+            else if(arr[i] < 50 && arr[i]%2 == 1){
                 arr[i] = arr[i]*2 + 1;
-                IsTrue = true;
+                diff = true;
             }
         }
-    }while(IsTrue);
-    return --answer;
+        ++x;
+        if(!diff){
+            return x-1;
+        }
+        diff = false;
+    }
 }
