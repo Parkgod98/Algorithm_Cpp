f16d48565eaff5fdbbd0e8b38e65a4c0fdbe8f2b
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 11:00:56 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 0.12 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.cpp
@@ -1,12 +1,20 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 string solution(string my_string, vector<vector<int>> queries) {
-	string answer = "";
-	for (vector<int>& v : queries) {
-		reverse(my_string.begin() + v[0], my_string.begin() + v[1] + 1);
-	}
-	return my_string;
+    
+    for (vector<int> &v : queries){
+        int s = v[0];
+        int e = v[1];
+        
+        int len = (e-s+1);
+        for (int i = 0; i < len/2; ++i){
+            char tmp = my_string[i + s];
+            my_string[i+s] = my_string[s + len -i - 1];
+            my_string[s + len -i - 1] = tmp;
+        }
+    }
+    return my_string;
 }
