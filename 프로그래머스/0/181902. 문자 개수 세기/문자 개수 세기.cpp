1ea204ca57857a2e1739034084927938aab34f24
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-21 10:36:04 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 vector<int> solution(string my_string) {
-	
-	vector<int> ans(52, 0);
-
-	for (char& c : my_string) {
-		if (c >= 'A' && c <= 'Z')
-			ans[c - 'A']++;
-		else if (c >= 'a' && c <= 'z')
-			ans[c - 'a' + 26]++;
-	}
-	
-	return ans;
+    vector<int> answer;
+    
+    vector<int> arr(52,0);
+    
+    for (char &c : my_string){
+        if(isupper(c)){
+            arr[c-'A']++;
+        }
+        else if(islower(c)){
+            arr[c-'a'+26]++;
+        }
+    }
+    return arr;
 }
