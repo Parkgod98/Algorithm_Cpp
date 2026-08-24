fb9c1da1a22aff367ddd5093a50487952118a868
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-24 09:06:51 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 5.2 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -1,24 +1,29 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <map>
+
 using namespace std;
 
 int solution(vector<int> rank, vector<bool> attendance) {
-	int answer = 0;
-
-	map<int,int> mp;
-	
-	for (int i = 0; i < attendance.size(); ++i) {
-		if (attendance[i]) {
-			mp[rank[i]] = i;
-		}
-	}
+    int answer = 0;
     
-	int k = 10000;
-	for (auto &m : mp) {
-		answer += k * m.second;
-		k /= 100;
-	}
-		return answer;
+    vector<int> v;
+    
+    int sz = attendance.size();
+    for (int i = 0; i < sz; ++i){
+        if(attendance[i])
+            v.push_back(i);
+    }
+    
+    for (int i = 0; i < v.size(); ++i){
+        for (int j = i+1; j < v.size(); ++j){
+            if(rank[v[i]] > rank[v[j]]){
+                int tmp = v[i];
+                v[i] = v[j];
+                v[j] = tmp;
+            }
+        }
+    }
+    
+    int sum = v[0]*10000 + v[1]*100 + v[2];
+    return sum;
 }
