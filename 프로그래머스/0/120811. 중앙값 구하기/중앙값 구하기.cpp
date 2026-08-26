2a1446b51d31d6192320e0a4406f04d5ad61e7d3
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 18:49:52 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -4,8 +4,18 @@
 using namespace std;
 
 int solution(vector<int> array) {
-    sort(array.begin(),array.end());
     
-    int sz = array.size();
-    return array[sz/2];
+    int visited[2002] = {0};
+    
+    for (int &n : array){
+        visited[n+1000]++;
+    }
+    int tar = array.size()/2;
+    
+    for (int i = 0; i < 2002; ++i){
+        tar -= visited[i];
+        
+        if(tar < 0)
+            return i-1000;
+    }
 }
