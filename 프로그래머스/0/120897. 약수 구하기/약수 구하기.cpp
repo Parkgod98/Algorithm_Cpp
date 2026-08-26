236b747d57990c915888dea1488b16561192a846
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 19:30:28 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -6,9 +6,17 @@ using namespace std;
 vector<int> solution(int n) {
     vector<int> ans;
     
+    int visited[10001] = {0};
+    
+    for (int i = 1; i *i <= n; ++i){
+        if(n%i == 0){
+            visited[i] = visited[n/i] = 1;
+        }
+    }
     for (int i = 1; i <= n; ++i){
-        if(n%i == 0)
+        if(visited[i])
             ans.push_back(i);
     }
+
     return ans;
 }
