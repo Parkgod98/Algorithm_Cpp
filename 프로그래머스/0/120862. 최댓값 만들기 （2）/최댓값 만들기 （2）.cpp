6ebac4780a2f5db39a2c27acd222a399fba7e5f8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 19:11:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.03 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cpp
@@ -1,15 +1,34 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> numbers) {
-    sort(numbers.begin(),numbers.end());
-    int a = numbers[0] * numbers[1];
     
-    int sz = numbers.size();
-    int b = numbers[sz-1] * numbers[sz-2];
+    int mx1 = -20000;
+    int mx2 = -19999;
+    int mn1 = 20000;
+    int mn2 = 20001;
     
-    return a > b ? a : b;
+    for (int &n : numbers){
+        if(n > mx1){ 
+            mx2 = mx1;
+            mx1 = n;
+        }
+        else if(mx1 >= n && mx2 <= n){
+            mx2 = n;
+        }
+        
+        if(n < mn1){
+            mn2 = mn1;
+            mn1 = n;
+        }
+        else if(mn1 <= n && mn2 >= n){
+            mn2 = n;
+        }
+    }
+    
+    return mx1*mx2 > mn1*mn2 ? mx1*mx2 : mn1*mn2;
 }
