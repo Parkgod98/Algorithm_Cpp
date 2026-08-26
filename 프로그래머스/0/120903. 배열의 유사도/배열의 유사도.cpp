2d6fec687a640979a2ae13e4a049e55608337526
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 19:39:16 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -1,19 +1,19 @@
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int solution(vector<string> s1, vector<string> s2) {
-    int res = 0;
+
+    set<string> s;
+    for (string &ss : s1)
+        s.insert(ss);
     
-    int s1_sz = s1.size();
-    int s2_sz = s2.size();
-    
-    for (int i = 0; i < s1_sz; ++i){
-        for (int j = 0; j < s2_sz; ++j){
-            if(s1[i] == s2[j])
-                ++res;
-        }
+    int cnt = 0;
+    for (string &ss : s2){
+        if(s.find(ss) != s.end())
+            ++cnt;
     }
-    return res;
+    return cnt;
 }
