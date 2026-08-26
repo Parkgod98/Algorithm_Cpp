499cd9484919f1532bf7f750d1e9d2b8deecaa85
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 19:25:17 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -4,13 +4,6 @@
 using namespace std;
 
 int solution(int chicken) {
-    int cnt = 0;
-    
-    while(chicken >=10){
-        cnt += chicken/10;
-        int remain = chicken%10;
-        chicken /= 10;
-        chicken += remain;
-    }
-    return cnt;
+
+    return (chicken-1)/9;
 }
