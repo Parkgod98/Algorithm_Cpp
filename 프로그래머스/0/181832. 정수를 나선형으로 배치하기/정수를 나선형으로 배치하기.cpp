cac7756c675270e656020b3372df27fb93a3d11c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 10:47:45 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.17 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -1,40 +1,33 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
-bool is_blocked(int y, int x, int n) {
-    if (y < 0 || x < 0 || y >= n || x >= n)
-        return true;
-    return false;
-}
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
 
 vector<vector<int>> solution(int n) {
-    vector<vector<int>> v(n, vector<int>(n, 0));
-
-    int y, x;
+    vector<vector<int>> ans(n,vector<int>(n,0));
+    
+    int i = 1;
+    
+    int sz = n*n;
+    
+    int y,x;
     y = x = 0;
-    int start = 1;
-    int dy[4] = { 0,1,0,-1 };
-    int dx[4] = { 1,0,-1,0 };
-    int i = 0;
-    while (start <= n*n) {
-        v[y][x] = start++;
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (!is_blocked(ny, nx, n) && v[ny][nx] == 0) {
-            y = ny;
-            x = nx;
-        }
-        else {
-            if (i == 3)
-                i = 0;
-            else 
-                i++;
-            y = y + dy[i];
-            x = x + dx[i];
-        }
+    int dir = 1;
+    while(i <= sz){
+        
+        ans[y][x] = i++;
+        
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n || ans[ny][nx] != 0)
+            dir = (dir +1)%4;
+        
+        y = y + dy[dir];
+        x = x + dx[dir];
     }
-
-    return v;
+    return ans;
 }
