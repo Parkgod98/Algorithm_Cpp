0d510ae7445c18862f13a9064c506629633407e0
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 19:44:30 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -1,12 +1,9 @@
 #include <string>
 #include <vector>
-
+#include <cmath>
 using namespace std;
 
 int solution(int n, int t) {
     
-    for (int i = 0; i < t; ++i){
-        n*=2;
-    }
-    return n;
+    return pow(2,t)*n;
 }
