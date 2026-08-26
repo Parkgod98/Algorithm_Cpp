9818e89ce8b46c9085efd1290f1414dde5acd36c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-26 15:08:18 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int sum = 0;
+    for (int i = 1; i *i <= n; ++i){
+        if(n%i == 0){
+            sum += i;
+            if(i*i != n)
+                sum += (n/i);
+        }
+    }
+    return sum;
+}
