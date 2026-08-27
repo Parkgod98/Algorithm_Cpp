937413a2f1e2cdbc715f806a7900ede4784102a0
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-27 13:04:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int cnt = 0;
+    
+    long long n = num;
+    while(n != 1){
+        if(n%2 == 0)
+            n /=2;
+        else
+            n = (n*3 + 1);
+        ++cnt;
+    }
+    return cnt < 500 ? cnt : -1;
+}
