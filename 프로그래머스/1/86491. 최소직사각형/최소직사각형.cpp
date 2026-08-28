9fa9a9d62263ae4aca39a6a602fc621d59dbe187
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-28 11:16:04 +0900

    [level 1] Title: 최소직사각형, Time: 0.43 ms, Memory: 5.57 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -1,25 +1,20 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <map>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 int solution(vector<vector<int>> sizes) {
-	for (auto& v : sizes) {
-		if (v[0] < v[1]) {
-			swap(v[0], v[1]);
-		}
-	}
-	int max1, max2;
-	max1 = max2 = 0;
-	for (int i = 0; i < sizes.size(); ++i) {
-		if (max1 < sizes[i][0])
-			max1 = sizes[i][0];
-		if (max2 < sizes[i][1])
-			max2 = sizes[i][1];
-	}
-
-	return max1 * max2;
+    int r = 0;
+    int c = 0;
+    for (vector<int> &v : sizes){
+        if(v[0] > v[1])
+            swap(v[0],v[1]);
+        
+        r = max(v[0],r);
+        c = max(v[1],c);
+    }
+    
+    return r*c;
 }
