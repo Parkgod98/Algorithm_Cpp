889d3692d4bd3837c71eb1bdcd650dd0a7fc1943
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 18:01:36 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -3,13 +3,16 @@
 
 using namespace std;
 
-string solution( int a, int b ) {
-    int days = 5;
-    vector<int> month = { 0,31,29,31,30,31,30,31,31,30,31,30,31 };
-    vector<string> ans = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
-    for (int i = 1; i < a; ++i ){
-        days += month[i];
-    }
-    days += b-1;
-    return ans[days%7];
+string solution(int a, int b) {
+    string answer = "";
+    
+    int days[13] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+    string day[7] = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+    
+    int start = 0;
+    
+    for (int i = 1; i < a; ++i)
+        start += days[i];
+    start += (b-1);
+    return day[start%7];
 }
