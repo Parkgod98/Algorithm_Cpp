76442642872d4f3fdea783b4c601c24d9a6ddad6
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 17:33:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -1,27 +1,29 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <cctype>
-#include <queue>
-#include <bitset>
+#include <cmath>
+
 using namespace std;
 
-
-vector<string> solution( int n, vector<int> arr1, vector<int> arr2 ) {
-    vector<string> answer;
-    for( int i = 0; i < n; ++i ){
-        int k = arr1[i] | arr2[i];
-        string s = "";
-        for( int j = 0; j < n; ++j ){
-            if( ( k & 1 ) == 1 )
-                s += "#";
-            else
-                s += " ";
-            k = k >> 1;
-        }
-        answer.push_back( string( s.rbegin(), s.rend() ) );
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
+    vector<string> ans;
+    
+    for (int i = 0; i < n; ++i){
+        arr1[i] |= arr2[i];
     }
-    return answer;
+
+    for (int k : arr1){
+        string s = "";
+        
+        int start = n - 1;
+        for (start; start >= 0; --start){
+            if((k & (1 << start)) == 0)
+                s += " ";
+            else
+                s += "#";
+                
+        }
+        ans.push_back(s);
+    }
+    return ans;
 }
