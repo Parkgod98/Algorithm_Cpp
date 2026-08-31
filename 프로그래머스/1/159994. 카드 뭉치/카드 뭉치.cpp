eb4796a48474edc262471f26dd4db36ed83a2689
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 16:15:12 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -3,17 +3,21 @@
 
 using namespace std;
 
-string solution( vector<string> cards1, vector<string> cards2, vector<string> goal ) {
-    int p1, p2;
-    p1 = p2 = 0;
-    for( int i = 0; i < goal.size(); ++i ){
-        if( p1 < cards1.size() && cards1[p1] == goal[i] ){
-            p1++;
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    int cur1 = 0;
+    int cur2 = 0;
+
+    for (string& word : goal) {
+        if (cur1 < cards1.size() && cards1[cur1] == word) {
+            ++cur1;
         }
-        else if( p2 < cards2.size() && cards2[p2] == goal[i] )
-            p2++;
-        else
+        else if (cur2 < cards2.size() && cards2[cur2] == word) {
+            ++cur2;
+        }
+        else {
             return "No";
+        }
     }
+
     return "Yes";
 }
