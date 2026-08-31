4a054ddc38a6080f4b625c787eb5d0da8767b8a8
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 17:39:48 +0900

    [level 1] Title: 추억 점수, Time: 1.03 ms, Memory: 5.14 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -1,25 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <map>
+
 using namespace std;
 
-
-vector<int> solution( vector<string> name, vector<int> yearning, vector<vector<string>> photo ) {
-    map<string, int> score_map;
-    vector<int> ans;
-    ans.reserve( photo.size() );
-    for( int i = 0; i < name.size(); ++i ){
-        score_map[name[i]] = yearning[i];
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    map<string,int> mp;
+    for (int i = 0; i < name.size(); ++i){
+        mp[name[i]] = yearning[i];
     }
-    for( vector<string>& v : photo ){
+    
+    vector<int> ans;
+    for (vector<string> &v : photo){
+        
         int sum = 0;
-        for( string& s : v ){
-            if( score_map.find( s ) != score_map.end() ){
-                sum += score_map[s];
-            }
+        for (string &s : v){
+            if(mp.find(s) != mp.end())
+                sum += mp[s];
         }
-        ans.emplace_back( sum );
+        ans.push_back(sum);
     }
     return ans;
 }
