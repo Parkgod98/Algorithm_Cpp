63bfcd0f631b894d3ce331d30700a8b674abd609
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 15:39:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.20 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -1,27 +1,19 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <cctype>
 #include <queue>
+
 using namespace std;
 
-
-vector<int> solution( int k, vector<int> score ) {
-    vector<int> answer;
+vector<int> solution(int k, vector<int> score) {
+    vector<int> ans;
     priority_queue<int,vector<int>, greater<int>> pq;
-    for( int i = 0; i < score.size(); ++i ){
-        if( pq.size() >= k ){
-            if( score[i] > pq.top() ){
-                pq.pop();
-                pq.push( score[i] );
-            }
-        }
-        else{
-            pq.push( score[i] );
-        }
-        answer.push_back( pq.top() );
+    
+    for (int &n :score){
+        pq.push(n);
+        if(pq.size() > k)
+            pq.pop();
+        
+        ans.push_back(pq.top());
     }
-    return answer;
+    return ans;
 }
