9d990c3cf13bc2639abe034a17ddea3bf14bd53c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-08-31 14:23:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.03 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,15 +1,28 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <deque>
+
 using namespace std;
 
-
 string solution(vector<int> food) {
-	string answer = "0";
-	for (int i = food.size() - 1; i > 0; --i) {
-		for (int j = 0; j < food[i]/2; ++j) {
-			answer = to_string(i) + answer + to_string(i);
-		}
-	}
-	return answer;
+    
+    deque<char> dq;
+    dq.push_back('0');
+    
+    int sz = food.size();
+    for (int i = sz-1; i >= 1; --i){
+        
+        int it = (food[i]/2)*2;
+        for (int k = 1; k <= it/2; ++k){
+            dq.push_front(i+'0');
+            dq.push_back(i+'0');
+        }
+    }
+    
+    string ans = "";
+    while(!dq.empty()){
+        ans += dq.front();
+        dq.pop_front();
+    }
+    return ans;
 }
