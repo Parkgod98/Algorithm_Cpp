e55098295a1c22145cb09ae1f296c25a659a6e8f
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-01 21:28:19 +0900

    [level 1] Title: 과일 장수, Time: 19.18 ms, Memory: 35.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -2,25 +2,18 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
-#include <sstream>
-#include <cctype>
-#include <queue>
-#include <bitset>
-#include <map>
-#include <set>
-#include <cmath>
+
 using namespace std;
 
-int solution( int k, int m, vector<int> score )
-{
-    int answer = 0;
-    sort( score.begin(), score.end(), greater<int>() );
-    for( int i = 0; i < score.size(); i+=m )
-    {
-        if( i + m <= score.size() )
-        {
-            answer += score[i+m-1]*m;
+int solution(int k, int m, vector<int> score) {
+    int ans = 0;
+    
+    sort(score.begin(),score.end());
+    int sz = score.size();
+    for (int i = sz - 1; i >= 0; i -= m){
+        if(i - m + 1 >= 0){
+            ans += score[i - m + 1]*m;
         }
     }
-    return answer;
+    return ans;
 }
