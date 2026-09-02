a7a1188635e01fc50934479b9c6ebf53985be5ad
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-02 19:07:21 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.cpp
@@ -1,68 +1,40 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <sstream>
 #include <cctype>
-#include <queue>
-#include <bitset>
-#include <map>
-#include <set>
-#include <cmath>
+
 using namespace std;
 
-#define CANT -9999999
-
-
 int solution(string dartResult) {
-     vector<int> score(3);
-
-     int idx = 0;
-     char prev = ' ';
-     for(int i = 0; i < dartResult.size(); ++i) {
-          if(isdigit(dartResult[i])) {
-               int n = 0;
-               while(isdigit(dartResult[i])) {
-                    n = (dartResult[i] - '0')  + n*10;
-                    i++;
-               }
-               i--;
-               score[idx] = n;
-          }
-          else if(isalpha(dartResult[i])) {
-               switch(dartResult[i]) {
-               case 'S':
-                    break;
-               case 'D':
-                    score[idx] = score[idx] * score[idx];
-                    break;
-               case 'T':
-                    score[idx] = score[idx] * score[idx] * score[idx];
-                    break;
-               }
-               idx++;
-          }
-          else {
-               if(dartResult[i] == '*') {
-                    if(idx == 1) // 첫 트라이에 나옴
-                    {
-                         score[idx - 1] *= 2;
-                    }
-                    else {
-                         score[idx - 2] *= 2;
-                         score[idx - 1] *= 2;
-                    }
-
-               }
-               else if(dartResult[i] == '#') {
-                    score[idx - 1] *= -1;
-               }
-               prev = dartResult[i];
-          }
-     }
-     int ans = 0;
-     for(int i = 0; i < score.size(); ++i) {
-          ans += score[i];
-     }
-     return ans;
+    int sz = dartResult.size();
+    vector<int> score;
+    
+    int num = 0;
+    for (int i = 0; i < sz; ++i){
+        char c = dartResult[i];
+        if(isdigit(c)){
+            num = num*10 + (c-'0');
+        }
+        else if(c == 'S' || c == 'D' || c == 'T'){
+            if(c == 'D')
+                num *= num;
+            else if(c == 'T')
+                num *= (num*num);
+            
+            score.push_back(num);
+            num = 0;
+        }
+        else if(c == '#'){
+            score[score.size()-1] *= -1;
+        }
+        else if(c == '*'){
+            score[score.size()-1] *=2;
+            if(score.size() >= 2)
+                score[score.size()-2]*=2;
+        }
+    }
+    
+    int sum = 0;
+    for (int &n : score)
+        sum += n;
+    return sum;
 }
