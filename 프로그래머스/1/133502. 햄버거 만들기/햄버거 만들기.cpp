fa2777ea9ab62d5d1fbc9551f5e2ef16d1d5a42f
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-03 13:48:18 +0900

    [level 1] Title: 햄버거 만들기, Time: 11.06 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -1,44 +1,28 @@
-#include <string>
 #include <vector>
-#include <queue>
+
 using namespace std;
 
 int solution(vector<int> ingredient) {
-    int cnt = 0;
-    deque<int> dq;
-    int state = 0;
-    for (int &n : ingredient){
-        if(!dq.empty()){
-            if(dq.back() == 1 && n == 2){
-                dq.push_back(n);
-                state = 1;
-            }
-            else if(dq.back() == 2 && n == 3 && state == 1){
-                dq.push_back(n);
-                state = 2;
-            }
-            else if(dq.back() == 3 && n == 1 && state == 2){
-                for (int i = 0; i < 3; ++i)
-                    dq.pop_back();
-                int cur = dq.size() - 1;
-                
-                if(cur -2 >= 0 && dq[cur-2] == 1 && dq[cur-1] == 2 && dq[cur] == 3)
-                    state = 2;
-                else if(cur-1 >= 0 && dq[cur-1] == 1 && dq[cur] == 2)
-                    state = 1;
-                else
-                    state = 0;
-                ++cnt;
-            }
-            else{
-                state = 0;
-                dq.push_back(n);
-            }
-        }
-        else{
-            state = 0;
-            dq.push_back(n);
+    vector<int> st;
+    int answer = 0;
+
+    for (int n : ingredient) {
+        st.push_back(n);
+
+        int sz = st.size();
+
+        if (sz >= 4 &&
+            st[sz - 4] == 1 &&
+            st[sz - 3] == 2 &&
+            st[sz - 2] == 3 &&
+            st[sz - 1] == 1) {
+
+            for (int i = 0; i < 4; ++i)
+                st.pop_back();
+
+            ++answer;
         }
     }
-    return cnt;
+
+    return answer;
 }
