b4140c6e1a8d21e77a9af74da89b2a5048ea4ac7
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-08 12:12:56 +0900

    [level 1] Title: 노란불 신호등, Time: 1.73 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cpp
@@ -1,48 +1,46 @@
-#include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-int GetGCD(int a, int b){
-    while(a%b != 0){
-        int tmp = a%b;
+int GetGCD(int a, int b) {
+    while (b != 0) {
+        int tmp = a % b;
         a = b;
         b = tmp;
     }
-    return b;
+
+    return a;
 }
 
 int solution(vector<vector<int>> signals) {
-    int sz = signals.size();
-    
-    int mx_term = 1;
-    
-    int one = signals[0][0] + signals[0][1] + signals[0][2];
-    int two = signals[1][0] + signals[1][1] + signals[1][2];
-    int lcm = (one * two) / (GetGCD(one,two));
-    
-    for (int i = 2; i < sz; ++i){
-        int cycle = signals[i][0] + signals[i][1] + signals[i][2];
-        lcm = (lcm * cycle) / (GetGCD(lcm,cycle));
-        
+    int lcm = 1;
+
+    for (const vector<int>& signal : signals) {
+        int cycle = signal[0] + signal[1] + signal[2];
+
+        int gcd = GetGCD(lcm, cycle);
+        lcm = lcm / gcd * cycle;
     }
-    
-    vector<int> arr(lcm+1,0);
-    
-    for (vector<int> &v : signals){
-        int start = v[0]+1;
-        int interval = v[0] + v[1] + v[2];
-        
-        for (int i = start; i < lcm; i += interval){
-            for (int j = i; j < i+v[1] && j < lcm; ++j)
-                arr[j]++;
+
+    for (int time = 1; time <= lcm; ++time) {
+        bool allYellow = true;
+
+        for (const vector<int>& signal : signals) {
+            int G = signal[0];
+            int Y = signal[1];
+            int cycle = signal[0] + signal[1] + signal[2];
+
+            int cur = (time - 1) % cycle;
+
+            if (!(G <= cur && cur < G + Y)) {
+                allYellow = false;
+                break;
+            }
         }
+
+        if (allYellow)
+            return time;
     }
-    
-    for (int i = 0; i < lcm; ++i){
-        if(arr[i] == sz)
-            return i;
-    }
+
     return -1;
 }
