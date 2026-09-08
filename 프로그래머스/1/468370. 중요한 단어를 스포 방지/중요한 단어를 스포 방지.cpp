340fad7d6f1d61809a06a63293b5058b3621fc8e
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-08 15:08:08 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 0.19 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cpp
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cpp
@@ -5,47 +5,54 @@
 using namespace std;
 
 int solution(string message, vector<vector<int>> spoiler_ranges) {
-    set<string> normalWords;
-    set<string> spoilerWords;
-
-    int start = 0;
-
-    while (start < message.size()) {
-        int end = message.find(' ', start);
-
-        if (end == string::npos)
-            end = message.size();
-
-        int wordEnd = end - 1;
-        bool isSpoiler = false;
-
-        for (const auto& range : spoiler_ranges) {
-            int spoStart = range[0];
-            int spoEnd = range[1];
-
-            // 두 구간이 겹친다
-            if (!(wordEnd < spoStart || spoEnd < start)) {
-                isSpoiler = true;
-                break;
-            }
+    set<string> normal_words;
+    set<string> spo_words;
+    
+    int idx = 0;
+    int prev = 0;
+    int sp_idx = 0;
+    bool Stop = false;
+    
+    while(idx != string::npos){
+        idx = message.find(' ',prev);
+        
+        if(idx == string::npos){
+            idx = message.size();
+            Stop = true;
         }
-
-        string word = message.substr(start, end - start);
-
-        if (isSpoiler)
-            spoilerWords.insert(word);
+        
+        string cur = message.substr(prev,idx-prev);
+        
+        while(sp_idx < (int)spoiler_ranges.size() && spoiler_ranges[sp_idx][1] < prev){
+            ++sp_idx;
+        }
+        
+        bool IsSpoiler = false;
+        if(sp_idx < (int)spoiler_ranges.size()){
+            int spo_start = spoiler_ranges[sp_idx][0];
+            int spo_end = spoiler_ranges[sp_idx][1];
+            
+            if(!(prev > spo_end || idx-1 < spo_start)){
+                IsSpoiler = true;
+            }
+            
+        }
+        
+        if(IsSpoiler){
+            spo_words.insert(cur);
+        }
         else
-            normalWords.insert(word);
-
-        start = end + 1;
+            normal_words.insert(cur);
+        
+        if(Stop)
+            break;
+        prev = idx+1;
     }
-
-    int answer = 0;
-
-    for (const string& word : spoilerWords) {
-        if (normalWords.find(word) == normalWords.end())
-            ++answer;
+    
+    int ans = 0;
+    for (const string &s : spo_words){
+        if(normal_words.find(s) == normal_words.end())
+            ++ans;
     }
-
-    return answer;
+    return ans;
 }
