b382dc2c0671e687374c0256f5e8b8e0d22c3bbc
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-11 14:13:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.06 ms, Memory: 6.61 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    
+    stack<char> st;
+    
+    for (char &c : s){
+        if(!st.empty()){
+            if(st.top() == c)
+                st.pop();
+            else
+                st.push(c);
+        }
+        else{
+            st.push(c);
+        }
+    }
+    
+    if(st.empty())
+        return 1;
+    return 0;
+}
