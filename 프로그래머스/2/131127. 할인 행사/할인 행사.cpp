dd0f0bdaff173f31acb3a403535b3ca54d72a5f1
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-14 16:04:51 +0900

    [level 2] Title: 할인 행사, Time: 11.71 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -5,31 +5,19 @@
 using namespace std;
 
 int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    
     int sz = want.size();
     
     int ans = 0;
     
     map<string,int> mp;
-    map<string,int> true_mp;
-    for (int i = 0; i < sz; ++i){
-        true_mp[want[i]] = number[i];
-    }
-    
     for (int i = 0; i < 10; ++i)
         mp[discount[i]]++;
-
     
     int dsz = discount.size();
-    for (int i = 1; i <= dsz-10; ++i){
-        
-        // for (auto &it : mp){
-        //     cout << it.first << " " << it.second << "\n";
-        // }
-        // cout << "\n";
+    for (int i = 0; i <= dsz-10; ++i){
         bool f = true;
         for (int k = 0; k < sz; ++k){
-            if(mp[want[k]] < true_mp[want[k]]){
+            if(mp[want[k]] < number[k]){
                 f = false;
                 break;
             }
@@ -37,18 +25,11 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
         if(f)
             ++ans;
         
-        mp[discount[i-1]]--;
-        mp[discount[i+10 -1]]++;
+        if(i == dsz - 10)
+            break;
+        
+        mp[discount[i]]--;
+        mp[discount[i+10]]++;
     }
-    bool f = true;
-    
-    for (int k = 0; k < sz; ++k){
-            if(mp[want[k]] < true_mp[want[k]]){
-                f = false;
-                break;
-            }
-        }
-    if(f)
-        ++ans;
     return ans;
 }
