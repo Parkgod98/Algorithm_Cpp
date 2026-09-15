0cda0ae655e719f427a16a4ae20b52d06de88853
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-15 17:56:58 +0900

    [level 2] Title: 전화번호 목록, Time: 259.11 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,29 +1,29 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <cctype>
-#include <queue>
-#include <bitset>
 #include <map>
-#include <set>
-#include <cmath>
-#include <unordered_map>
-#include <stack>
-#include <deque>
-#include <list>
+#include <iostream>
+
 using namespace std;
 
-
-bool solution( vector<string> phone_book )
-{
-    sort( phone_book.begin(), phone_book.end() );
-
-    for( int i = 0; i < phone_book.size() - 1; ++i )
-    {
-        if( phone_book[i + 1].find( phone_book[i] ) == 0 )
-            return false;
+bool solution(vector<string> phone_book) {
+    
+    map<string,int> mp;
+    vector<int> visited(21);
+    for (string &s : phone_book){
+        visited[s.size()] = 1;
+        mp[s] = 1;
+    }
+    
+    for (string &s : phone_book){
+        
+        for (int i = 1; i <= 20; ++i){
+            if(visited[i]){
+                string ss = s.substr(0,i);
+                auto it = mp.find(ss);
+                if(it != mp.end() && it->first != s)
+                    return false;
+            }
+        }
     }
     return true;
 }
