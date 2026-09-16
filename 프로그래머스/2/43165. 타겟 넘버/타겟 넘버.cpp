3ad7cf2e2140e698a644730887c5251a29cc7bcb
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-17 08:39:35 +0900

    [level 2] Title: 타겟 넘버, Time: 2.23 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,41 +1,23 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <sstream>
-#include <cctype>
-#include <queue>
-#include <bitset>
-#include <map>
-#include <set>
-#include <cmath>
-#include <unordered_map>
-#include <stack>
-#include <deque>
-#include <list>
-#include <unordered_set>
+
 using namespace std;
-
-int dy[4] = { -1,0,1,0 };
-int dx[4] = { 0,1,0,-1 };
-
-int cnt = 0;
-
-void DFS( int sum, int &target, int depth, vector<int>& numbers )
-{
-    if( depth == numbers.size())
-    {
-        if( target == sum )
-            ++cnt;
+int ans = 0;
+void DFS(vector<int> &v, int tar, int sum, int idx){
+    
+    if(idx == v.size()){
+        if(sum == tar)
+            ++ans;
         return;
-
     }
-    DFS( sum + numbers[depth], target, depth + 1, numbers );
-    DFS( sum - numbers[depth], target, depth + 1, numbers );
+    
+    DFS(v,tar,sum+v[idx],idx+1);
+    DFS(v,tar,sum-v[idx],idx+1);
+    
 }
 
-int solution( vector<int> numbers, int target )
-{
-    DFS( 0, target, 0, numbers );
-    return cnt;
+int solution(vector<int> numbers, int target) {
+    
+    DFS(numbers,target,0,0);
+    return ans;
 }
