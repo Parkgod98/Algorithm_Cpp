fffe36937c0938d14d3db24a77804f01d0f5292e
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-18 13:30:09 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 3.59 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -1,40 +1,47 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
 using namespace std;
 
-typedef long long ll;
-
 vector<int> solution(vector<int> sequence, int k) {
-	vector<int> answer;
-	vector<int> prefix_sum(sequence.size()+1, 0);
-
-	for (int i = 1; i < prefix_sum.size(); ++i) {
-		prefix_sum[i] = prefix_sum[i - 1] + sequence[i-1];
-	}
-	int i, j;
-	i = 0;
-	j = 1;
-	int min_i, min_j, min_len;
-	min_i = min_j = min_len = 1000001;
-
-	while (j < prefix_sum.size() && i < j) {
-		int j_1_i = prefix_sum[j] - prefix_sum[i];
-		if (j_1_i > k) {
-			i++;
-		}
-		else if (j_1_i < k)
-			j++;
-		if (j_1_i == k) {
-			if (min_len > j - i) {
-				min_len = j - i;
-				min_j = j;
-				min_i = i;
-			}
-			j++;
-		}
-	}
-	vector<int> v(2);
-	v[0] = min_i;
-	v[1] = min_j - 1;
-
-	return v;
+    int mn = 10000000;
+    int ss,ee;
+    ss = ee = 1000000;
+    int s = 0;
+    int e = 1;
+    int sum = sequence[s];
+    int sz = sequence.size();
+    while(s < sz && e <= sz){
+        // cout << "s : " << s << " e : " << e << " sum : " << sum << "\n";
+        if(sum == k){
+            int cur_len = e - s;
+            if(cur_len < mn){
+                mn = cur_len;
+                ss = s;
+                ee = e;
+            }
+            else if(cur_len == mn && s < ss){
+                ss = s;
+                ee = e;
+            }
+            sum -= sequence[s];
+            ++s;
+        }
+        else if(sum > k){
+            sum -= sequence[s];
+            ++s;
+        }
+        else{ // sum < k
+            if(e >= sz)
+                break;
+            sum += sequence[e];
+            ++e;
+        }
+    }
+    
+    vector<int> ans(2,0);
+    ans[0] = ss;
+    ans[1] = ee-1;
+    return ans;
 }
