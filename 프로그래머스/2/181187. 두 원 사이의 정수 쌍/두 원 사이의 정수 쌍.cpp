e2254d675c850e2b96f97be3612ddece69de1487
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-25 01:45:01 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 1.86 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+long long solution(int r1, int r2) {
+    ll cnt = 0;
+    
+    ll j = r2+1;
+    
+    ll rr1 = r1;
+    ll rr2 = r2;
+    for (ll i = 1; i < r2; ++i){
+        
+        while(!(i*i + j*j <= rr2*rr2))
+            --j;
+        
+        ll start = 1;
+        if(i < r1)
+            start = (ll)(ceil(sqrt(rr1*rr1 - i*i)));
+            
+        if(start != 0)
+            cnt += (j - start) + 1;
+        else
+            cnt += (j-start);
+    }
+    
+    cnt*=4;
+    cnt += (r2-r1+1)*4 ;
+    return cnt;
+}
