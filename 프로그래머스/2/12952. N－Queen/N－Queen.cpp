d47cdfcad9ac4452c1db122961b693499a6a9914
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-24 20:19:14 +0900

    [level 2] Title: N-Queen, Time: 44.89 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.cpp
+++ 프로그래머스/2/12952. N－Queen/N－Queen.cpp
@@ -1,40 +1,47 @@
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int cnt = 0;
 
-void DFS(int n, int r, vector<int>& queen) {
+void DFS(int n, int r,
+         vector<int>& col,
+         vector<int>& diag1,
+         vector<int>& diag2) {
+
     if (r == n) {
         ++cnt;
         return;
     }
 
     for (int c = 0; c < n; ++c) {
-        bool valid = true;
 
-        for (int prev = 0; prev < r; ++prev) {
-            if (queen[prev] == c ||
-                abs(r - prev) == abs(c - queen[prev])) {
-                valid = false;
-                break;
-            }
-        }
+        int d1 = r - c + n - 1;
+        int d2 = r + c;
 
-        if (!valid)
+        if (col[c] || diag1[d1] || diag2[d2])
             continue;
 
-        queen[r] = c;
-        DFS(n, r + 1, queen);
+        col[c] = 1;
+        diag1[d1] = 1;
+        diag2[d2] = 1;
+
+        DFS(n, r + 1, col, diag1, diag2);
+
+        col[c] = 0;
+        diag1[d1] = 0;
+        diag2[d2] = 0;
     }
 }
 
 int solution(int n) {
     cnt = 0;
 
-    vector<int> queen(n, -1);
-    DFS(n, 0, queen);
+    vector<int> col(n, 0);
+    vector<int> diag1(2 * n - 1, 0);
+    vector<int> diag2(2 * n - 1, 0);
+
+    DFS(n, 0, col, diag1, diag2);
 
     return cnt;
 }
