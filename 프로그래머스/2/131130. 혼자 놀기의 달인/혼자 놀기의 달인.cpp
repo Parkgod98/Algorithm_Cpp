9b72e06713b77413db9cce335dd248c480779fd5
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-25 16:28:45 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.03 ms, Memory: 4.75 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cpp
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace std;
 
-int DFS(int cur, vector<int> &v, vector<int> &visited){
+int DFS(int cur, vector<int> &v, vector<int> &visited, int t){
     int sum = 1;
     
-    if(!visited[v[cur]-1]){
-        visited[v[cur]-1] = 1;
-        sum += DFS(v[cur]-1,v,visited);
+    if(visited[v[cur]-1] != t){
+        visited[v[cur]-1] = t;
+        sum += DFS(v[cur]-1,v,visited,t);
     }
     
     return sum;
@@ -19,22 +19,20 @@ int solution(vector<int> cards) {
     int a = 0;
     int b = 0;
     int mx = 0;
+    vector<int> visited(sz,0);
+    
+    int turn = 0;
     for (int i = 0; i < sz; ++i){
-        vector<int> visited(sz,0);
-        visited[i] = 1;
-        a = DFS(i,cards,visited);
-        // cout << a << " ";
-        // cout << "\n";
+        ++turn;
+        visited[i] = turn;
+        a = DFS(i,cards,visited,turn);
         for (int j = i + 1; j < sz; ++j){
-            if(!visited[j]){
-                visited[j] = 1;
-                b = DFS(j,cards,visited);
-                // cout << b << " ";
+            if(visited[j] != turn){
+                visited[j] = turn;
+                b = DFS(j,cards,visited,turn);
                 mx = max(mx,a*b);
             }
         }
-        
-        // cout << "\n\n";
     }
     
     return mx;
