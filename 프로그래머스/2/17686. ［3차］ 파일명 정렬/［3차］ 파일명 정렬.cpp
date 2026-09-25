f1832c6837d8bd9b10c8508bcf2089303e7d770c
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-26 02:40:29 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 0.50 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.cpp
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.cpp
@@ -4,46 +4,55 @@
 #include <iostream>
 using namespace std;
 
+
+struct File{
+    string origin;
+    string head;
+    int num;
+    int order;
+    
+    
+    bool operator<(const File &other) const{
+        if(head != other.head)
+            return head < other.head;
+        if(num != other.num)
+            return num < other.num;
+        return order < other.order;
+    }
+};
+
+
+
 vector<string> solution(vector<string> files) { 
     vector<string> ans;
     
     vector<string> head;
     vector<int> num;
     
+    vector<File> v;
+    int o = 0;
     for (string &s : files){
-        
         int idx = 0;
         while(idx < s.size() && !isdigit(s[idx]))
             ++idx;
         
+        File f;
+        f.origin = s;
+        f.order = o++;
         string hp = s.substr(0,idx);
         for (char &c : hp)
             c = tolower(c);
-        head.push_back(hp);
+        f.head = hp;
         int st = idx;
         while(idx < s.size() && isdigit(s[idx]) && idx - st < 5)
             ++idx;
-        
-        num.push_back(stoi(s.substr(st,idx-st)));
+        f.num = stoi(s.substr(st,idx-st));
+        v.push_back(f);
     }
+    sort(v.begin(),v.end());
     
-    int sz = files.size();
-    for (int i = 0; i < sz - 1; ++i){
-        for (int j = 0; j < sz - i - 1; ++j){
-            if(head[j] > head[j+1]){
-                swap(head[j],head[j+1]);
-                swap(files[j],files[j+1]);
-                swap(num[j],num[j+1]);
-            }
-            else if(head[j] == head[j+1]){
-                if(num[j] > num[j+1]){
-                    swap(head[j],head[j+1]);
-                    swap(files[j],files[j+1]);
-                    swap(num[j],num[j+1]);
-                }
-            }
-        }
-    }
+    for (auto &it : v)
+        ans.push_back(it.origin);
     
-    return files;
+    return ans;
 }
