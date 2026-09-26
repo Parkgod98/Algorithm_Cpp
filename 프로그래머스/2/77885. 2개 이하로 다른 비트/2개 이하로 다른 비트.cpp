e414d94639e66af1b3864ab55709041ab1d27dbb
Parkgod98 <wp3wktlfgdj@kookmin.ac.kr>
2026-09-26 23:16:31 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 26.03 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -5,33 +5,10 @@
 using namespace std;
 typedef long long ll;
 
-bool UnderTwo(ll a, ll b){
-    a ^= b;
-    int cnt = 0;
-    while(a != 0){
-        if(a%2 == 1){
-            ++cnt;
-        }
-        
-        a = (a >> 1);
-        if(cnt > 2)
-            return false;
-    }
-    return true;
-}
-
-/*
-
-
-*/
-
-
 vector<long long> solution(vector<long long> numbers) {
     vector<long long> ans;
     
     for (long long &n : numbers){
-        
-        
         if(n%2 == 0)
             ans.push_back(n+1);
         else{
